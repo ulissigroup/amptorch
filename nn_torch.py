798fcff56e0bf41664a30022cdc27ec623b2f98b
mshuaibi <mshuaibi@andrew.cmu.edu>
2019-02-06 16:47:51 -0500

    began writing the collate function

--- nn_torch.py
+++ nn_torch.py
@@ -25,12 +25,33 @@ class AtomsDataset(Dataset):
 
     def __getitem__(self,index):
         hash_name=self.atom_images.keys()[index]
-        image=self.atom_images.values()[index]
-        self.descriptor.calculate_fingerprints(self.atom_images)
         image_fingerprint=self.descriptor.fingerprints[hash_name]
-        #total vs potential energy?
-        image_energy=self.atom_images[hash_name].get_total_energy()
-        return image_fingerprint,image_energy
+        image_potential_energy=self.atom_images[hash_name].get_potential_energy()
+        return image_fingerprint,image_potential_energy
 
-test_data=AtomsDataset(descriptor=Gaussian())
-fp,E=test_data.__getitem__(19)
+training_data=AtomsDataset(descriptor=Gaussian())
+sample=training_data[19]
+#print(sample)
+
+
+
+
+def identify_unique_atoms(training_data):
+    unique_atoms=[]
+    for sample in training_data:
+        atom_image=sample[0]
+        image_potential_energy=sample[1]
+        for atom in atom_image:
+            element=atom[0]
+            if element not in unique_atoms:
+                unique_atoms.append(element)
+    return unique_atoms
+
+def collate_amp(training_data):
+    unique_atoms=identify_unique_atoms(training_data)
+
+unique_atoms=identify_unique_atoms(training_data)
+print(unique_atoms)
+
+#collate_amp(training_data)
+#atoms_dataloader=DataLoader(test_data,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=0,collate_fn=,pin_memory=False,drop_last=False,timeout=0,worker_init_fn=None)
