9e8b02b2542a6b14c3261c6b4312db9d6cf0ead2
mshuaibi <mshuaibi@andrew.cmu.edu>
2019-02-08 16:48:23 -0500

    collate updates

--- nn_torch.py
+++ nn_torch.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 from ase import Atoms
 import torch
-
+import copy
 """Loading Data"""
 from torch.utils.data import Dataset, DataLoader
 from amp.utilities import hash_images
@@ -30,28 +30,62 @@ class AtomsDataset(Dataset):
         return image_fingerprint,image_potential_energy
 
 training_data=AtomsDataset(descriptor=Gaussian())
-sample=training_data[19]
-#print(sample)
+sample=training_data[0]
+sample_batch=[training_data[i] for i in range(2)]
 
 
-
+def collate_amp(training_data):
+    unique_atoms,fingerprint_dict,energy_dict=identify_unique_atoms(training_data)
+    element_specific_fingerprint={}
+    for element_type in unique_atoms:
+        element_specific_fingerprint[element_type]=[]
+    for index,fingerprint_sample in enumerate(fingerprint_dict.values()):
+        for atom_fingerprint in fingerprint_sample:
+            atom_fingerprint[1].append(index)
+            for element in unique_atoms:
+                if atom_fingerprint[0]==element:
+                    element_specific_fingerprint[element].append(atom_fingerprint[1])
+    return element_specific_fingerprint
 
 def identify_unique_atoms(training_data):
     unique_atoms=[]
-    for sample in training_data:
+    fingerprint_dict={}
+    energy_dict={}
+    #Create empty dictionary to store indices of data
+    for index,sample in enumerate(training_data):
         atom_image=sample[0]
+        fingerprint_dict[index]=atom_image
         image_potential_energy=sample[1]
+        energy_dict[index]=image_potential_energy
         for atom in atom_image:
             element=atom[0]
             if element not in unique_atoms:
                 unique_atoms.append(element)
-    return unique_atoms
+    return unique_atoms,fingerprint_dict,energy_dict
 
-def collate_amp(training_data):
-    unique_atoms=identify_unique_atoms(training_data)
 
-unique_atoms=identify_unique_atoms(training_data)
-print(unique_atoms)
 
-#collate_amp(training_data)
+unique_atoms, fingerprint_dict, energy =identify_unique_atoms(sample_batch)
+#print fingerprint_dict[19]
+
+
+test=collate_amp(sample_batch)
+print(test)
+#Cu_test=test['Cu']
+# print('')
+# Pt_test=test['Pt']
+# print(len(Cu_test))
+# print(len(Pt_test))
+# dataloader=DataLoader(sample_batch,batch_size=1,collate_fn=collate_amp,shuffle=True)
+# for i in dataloader:
+    # print i
+    # sys.exit()
+    # print len(i)
+    # sys.exit()
+
+
+
+
 #atoms_dataloader=DataLoader(test_data,batch_size=1,shuffle=False,sampler=None,batch_sampler=None,num_workers=0,collate_fn=,pin_memory=False,drop_last=False,timeout=0,worker_init_fn=None)
+
+
