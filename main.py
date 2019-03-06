eafae811a9f24a81be4b612917ea0d60ce5e37a6
Muhammed <mshuaibi@andrew.cmu.edu>
2019-03-06 16:01:23 -0500

    cuda

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ from ase.calculators.emt import EMT
 # device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # print device
 print torch.cuda.is_available()
-cuda=torch.device('cuda')
-print cuda
 sys.exit()
 
 training_data=AtomsDataset('sample_training_data.traj',descriptor=Gaussian())
