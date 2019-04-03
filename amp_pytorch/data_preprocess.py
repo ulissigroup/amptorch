implemented variable sample length, corrected RMSE per atom calculation

amp_pytorch/data_preprocess.py
@@ -83,16 +83,18 @@ def factorize_data(training_data):
     unique_atoms = []
     fingerprint_dataset = []
     energy_dataset = []
+    num_of_atoms = []
     for image_sample in training_data:
         image_fingerprint = image_sample[0]
         fingerprint_dataset.append(image_fingerprint)
         image_potential_energy = image_sample[1]
         energy_dataset.append(image_potential_energy)
+        num_of_atoms.append(float(len(image_fingerprint)))
         for atom in image_fingerprint:
             element = atom[0]
             if element not in unique_atoms:
                 unique_atoms.append(element)
-    return unique_atoms, fingerprint_dataset, energy_dataset
+    return unique_atoms, fingerprint_dataset, energy_dataset, num_of_atoms
 
 
 def collate_amp(training_data):
@@ -101,7 +103,7 @@ def collate_amp(training_data):
     specific datasets to be fed into the element specific Neural Nets.
     """
 
-    unique_atoms, fingerprint_dataset, energy_dataset = factorize_data(
+    unique_atoms, fingerprint_dataset, energy_dataset, num_of_atoms = factorize_data(
         training_data)
     element_specific_fingerprints = {}
     model_input_data = []
@@ -119,4 +121,5 @@ def collate_amp(training_data):
             element_specific_fingerprints[element][0])
     model_input_data.append(element_specific_fingerprints)
     model_input_data.append(torch.tensor(energy_dataset))
+    model_input_data.append(torch.tensor(num_of_atoms))
     return model_input_data

amp_pytorch/nn_torch_bfgs.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import torch
 import torch.nn as nn
@@ -196,6 +197,7 @@ def train_model(model, unique_atoms, dataset_size, criterion, optimizer, atoms_d
             batch_size = len(target)
             target = target.reshape(batch_size, 1)
             scaled_target = target_scaling(target, method='standardize')
+            num_of_atoms = data_sample[2].reshape(batch_size, 1)
             # Send inputs and labels to the corresponding device (cpu or gpu)
             for element in unique_atoms:
                 input_data[element][0] = input_data[element][0].to(device)
@@ -215,8 +217,10 @@ def train_model(model, unique_atoms, dataset_size, criterion, optimizer, atoms_d
                 scaled_preds = model(input_data)
                 raw_preds = pred_scaling(
                     scaled_preds, target, method='standardize')
-                loss = criterion(raw_preds, target)
-                MSE += loss.item()*batch_size/len(unique_atoms)**2
+                raw_preds_per_atom = torch.div(raw_preds, num_of_atoms)
+                target_per_atom = torch.div(target, num_of_atoms)
+                loss = criterion(raw_preds_per_atom, target_per_atom)
+                MSE += loss.item()*batch_size
 
         MSE = MSE/dataset_size
         RMSE = np.sqrt(MSE)