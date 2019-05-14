resolved force training bug. Fingerprint derivative scaling removed

amp_pytorch/NN_model.py
@@ -6,7 +6,7 @@ import sys
 import time
 import numpy as np
 from collections import defaultdict, OrderedDict
-import torch 
+import torch
 import torch.nn as nn
 from torch.nn import init
 from torch.nn import Tanh, Softplus, LeakyReLU
@@ -38,14 +38,14 @@ class Dense(nn.Linear):
     def reset_parameters(self):
         """Weight initialization scheme"""
         # init.constant_(self.weight, 0.5)
-        # init.constant_(self.bias, 0.5)
+        init.constant_(self.bias, 0)
 
-        # xavier_uniform_(self.weight,gain=5.0/3)
+        # xavier_uniform_(self.weight, gain=np.sqrt(1/2))
         kaiming_uniform_(self.weight, nonlinearity="tanh")
-        if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / np.sqrt(fan_in)
-            init.uniform_(self.bias, -bound, bound)
+        # if self.bias is not None:
+            # fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            # bound = 1 / np.sqrt(fan_in)
+            # init.uniform_(self.bias, -bound, bound)
 
     def forward(self, inputs):
         neuron_output = super(Dense, self).forward(inputs)
@@ -83,8 +83,9 @@ class MLP(nn.Module):
             Dense(self.n_neurons[i], self.n_neurons[i + 1], activation=activation)
             for i in range(n_layers - 1)
         ]
-        layers.append(Dense(self.n_neurons[-2], self.n_neurons[-1],
-                            activation=None))
+        layers.append(
+            Dense(self.n_neurons[-2], self.n_neurons[-1], activation=None)
+        )
         self.model_net = nn.Sequential(*layers)
 
     def forward(self, inputs):
@@ -117,7 +118,13 @@ class FullNN(nn.Module):
         n_hidden_size = architecture[2]
         elementwise_models = nn.ModuleList()
         for n_models in range(n_unique_atoms):
-            elementwise_models.append(MLP(n_input_nodes=input_length, n_layers=n_layers, n_hidden_size=n_hidden_size))
+            elementwise_models.append(
+                MLP(
+                    n_input_nodes=input_length,
+                    n_layers=n_layers,
+                    n_hidden_size=n_hidden_size,
+                )
+            )
         self.elementwise_models = elementwise_models
         log("Activation Function = %s" % elementwise_models[0].activation)
 
@@ -135,6 +142,7 @@ class FullNN(nn.Module):
         fprimes = fprimes.to(self.device)
         # Constructs an Nx1 empty tensor to store element energy contributions
         dE_dFP_ = defaultdict(list)
+        x = torch.tensor([])
         for index, element in enumerate(self.unique_atoms):
             model_inputs = input_data[element][0]
             contribution_index = torch.tensor(input_data[element][1]).to(self.device)
@@ -145,7 +153,6 @@ class FullNN(nn.Module):
                 model_inputs,
                 grad_outputs=torch.ones_like(energy_pred),
                 create_graph=True,
-                # retain_graph=True,
             )[0]
             for fprime, con in zip(gradients, contribution_index):
                 dE_dFP_[int(con)].append(fprime)
@@ -155,14 +162,15 @@ class FullNN(nn.Module):
         dE_dFP = dE_dFP.reshape(1, -1)
         # Constructs a 1xPQ tensor that contains the derivatives with respect to
         # each atom's fingerprint
-        # force_pred = -1*torch.sparse.mm(fprimes.t(), dE_dFP.t())
-        force_pred = -1*torch.mm(dE_dFP, fprimes)
+        # force_pred = -1 * torch.sparse.mm(fprimes.t(), dE_dFP.t())
+        force_pred = -1 * torch.mm(dE_dFP, fprimes)
         # Sparse multiplication requires the first matrix to be sparse
         # Multiplies a 3QxPQ tensor with a PQx1 tensor to return a 3Qx1 tensor
         # containing the x,y,z directional forces for each atom
         force_pred = force_pred.reshape(3, -1).t()
         # Reshapes the force tensor into a Qx3 matrix containing all the force
         # predictions in the same order and shape as the target forces calculated from AMP.
+        # force_pred=0
         return energy_pred, force_pred
 
 
@@ -180,8 +188,8 @@ class ForceLossFunction(nn.Module):
         force_pred_per_atom = torch.div(force_pred, num_atoms_force)
         force_targets_per_atom = torch.div(force_targets, num_atoms_force)
         alpha = 0.04
-        MSE_loss = nn.MSELoss(reduction='sum')
-        loss = MSE_loss(energy_per_atom, targets_per_atom) + (alpha / 3) * MSE_loss(
-            force_pred_per_atom, force_targets_per_atom
-        )
+        MSE_loss = nn.MSELoss(reduction="sum")
+        energy_loss = MSE_loss(energy_per_atom, targets_per_atom)
+        force_loss = (alpha/3)*MSE_loss(force_pred_per_atom, force_targets_per_atom)
+        loss = energy_loss + force_loss
         return loss