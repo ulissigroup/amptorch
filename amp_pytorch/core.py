resolved force training bug. Fingerprint derivative scaling removed
Former-commit-id: 2e1328469d52b4d5315ebac3386137dc56a4464a

amp_pytorch/core.py
@@ -15,7 +15,7 @@ from amp.descriptor.gaussian import Gaussian
 import matplotlib.pyplot as plt
 from amp_pytorch.data_preprocess import AtomsDataset, factorize_data, collate_amp
 from amp_pytorch.NN_model import FullNN
-from amp_pytorch.trainer_core import train_model, pred_scaling
+from amp_pytorch.trainer import train_model, pred_scaling
 
 __author__ = "Muhammed Shuaibi"
 __email__ = "mshuaibi@andrew.cmu.edu"
@@ -165,32 +165,68 @@ class AMPtorch:
         fig.savefig("results/parity_plot.pdf")
         plt.show()
 
+    def parity_plot_forces(self, model):
+
+        model.eval()
+        targets = []
+        device = self.device
+        model = model.to(device)
+        for sample in self.atoms_dataloader:
+            inputs = sample[0]
+            fp_primes = sample[3]
+            for element in self.unique_atoms:
+                inputs[element][0] = inputs[element][0].to(device).requires_grad_(True)
+            targets = sample[1]
+            force_targets = sample[4]
+            targets = targets.to(device)
+            force_targets = force_targets.to(device)
+            inputs = [inputs, len(targets)]
+            energy_pred, force_pred = model(inputs, fp_primes)
+        force_pred = force_pred.reshape(-1, 1)
+        force_targets = force_targets.reshape(-1, 1)
+        data_min = min(force_targets)
+        data_max = max(force_targets)
+        fig = plt.figure(figsize=(7.0, 7.0))
+        ax = fig.add_subplot(111)
+        force_targets = force_targets.detach().cpu().numpy()
+        force_pred = force_pred.detach().cpu().numpy()
+        ax.plot(force_targets, force_pred, "bo", markersize=3)
+        ax.plot([data_min, data_max], [data_min, data_max], "r-", lw=0.3)
+        ax.set_xlabel("ab initio forces, eV/A")
+        ax.set_ylabel("PyTorch forces, eV/A")
+        ax.set_title("Forces")
+        fig.savefig("results/parity_plot_forces.pdf")
+        plt.show()
+
     def plot_residuals(self, model):
         """Plots model residuals"""
 
         model.eval()
-        predictions = []
         targets = []
         device = self.device
         model = model.to(device)
-        with torch.no_grad():
-            for sample in self.atoms_dataloader:
-                inputs = sample[0]
-                for element in self.unique_atoms:
-                    inputs[element][0] = inputs[element][0].to(device)
-                targets = sample[1]
-                targets = targets.to(device)
-                predictions = model(inputs)
-        scaled_pred = pred_scaling(predictions, targets, method="standardize")
-        targets = targets.reshape(len(targets), 1)
-        residuals = targets - scaled_pred
+        for sample in self.atoms_dataloader:
+            inputs = sample[0]
+            fp_primes = sample[3]
+            for element in self.unique_atoms:
+                inputs[element][0] = inputs[element][0].to(device).requires_grad_(True)
+            targets = sample[1]
+            force_targets = sample[4]
+            targets = targets.to(device)
+            force_targets = force_targets.to(device)
+            inputs = [inputs, len(targets)]
+            energy_pred, force_pred = model(inputs, fp_primes)
+        force_pred = force_pred.reshape(-1, 1)
+        force_targets = force_targets.reshape(-1, 1)
+        residual = abs(force_targets-force_pred)
         fig = plt.figure(figsize=(7.0, 7.0))
         ax = fig.add_subplot(111)
-        scaled_pred = scaled_pred.detach().cpu().numpy()
-        residuals = residuals.detach().cpu().numpy()
-        ax.plot(scaled_pred, residuals, "bo", markersize=3)
-        ax.set_xlabel("PyTorch energy, eV")
-        ax.set_ylabel("residual, eV")
-        ax.set_title("Energies")
-        fig.savefig("results/residuals_plot.pdf")
+        force_targets = force_targets.detach().cpu().numpy()
+        force_pred = force_pred.detach().cpu().numpy()
+        residual = residual.detach().cpu().numpy()
+        ax.plot(force_targets, residual, "bo", markersize=3)
+        ax.set_xlabel("DFT force, eV/A")
+        ax.set_ylabel("|DFT force - PyTorch force|, eV/A")
+        ax.set_title("Forces")
+        fig.savefig("results/residual_plot_forces.pdf")
         plt.show()

amp_pytorch/trainer.py
@@ -1,15 +1,16 @@
-"""trainer.py: Trains a provided model utilizing an LBFGS algorithm. Model
-convergence is achieved upon reaching a specific rmse convergence criteria."""
+"""GD_trainer.py: Trains a provided model utilizing GD based algorithms
+including SGD, Adam, etc. Model convergence is achieved upon reaching the
+specified rmse convergence criteria."""
 
 import sys
 import time
 import copy
 from amp.utilities import Logger
 import matplotlib.pyplot as plt
-import torch.nn
-from amp_pytorch.NN_model import ForceLossFunction
+import torch.nn as nn
 import torch
 import numpy as np
+from amp_pytorch.NN_model import ForceLossFunction
 
 __author__ = "Muhammed Shuaibi"
 __email__ = "mshuaibi@andrew.cmu.edu"
@@ -22,21 +23,15 @@ def target_scaling(data, method=None):
                 'standardize' = scales values to have a mean of 0 and standard
                 deviation of 1
         """
-
+    data_ = data.reshape(-1, 1)
     if method == "minmax":
-        data_max = max(data)
-        data_min = min(data)
-        scale = []
-        for value in data:
-            scale.append((value - data_min) / (data_max - data_min))
-        return torch.stack(scale)
+        data_max = max(data_)
+        data_min = min(data_)
+        data = (data - data_min) / (data_max - data_min)
     elif method == "standardize":
-        data_mean = torch.mean(data)
-        data_sd = torch.std(data, dim=0)
-        scale = []
-        for value in data:
-            scale.append((value - data_mean) / data_sd)
-        return torch.stack(scale)
+        data_mean = torch.mean(data_)
+        data_sd = torch.std(data_, dim=0)
+        data = (data - data_mean) / data_sd
     return data
 
 
@@ -45,20 +40,15 @@ def pred_scaling(data, target, method=None):
     Unscaling is necessary in order to compute RMSE values off the raw targets
     """
 
+    target = target.reshape(-1, 1)
     if method == "minmax":
         target_max = max(target)
         target_min = min(target)
-        scale = []
-        for value in data:
-            scale.append((value * (target_max - target_min)) + target_min)
-        return torch.stack(scale)
+        data = ((data * (target_max - target_min)) + target_min)
     elif method == "standardize":
         target_mean = torch.mean(target)
         target_sd = torch.std(target, dim=0)
-        scale = []
-        for value in data:
-            scale.append((value * target_sd) + target_mean)
-        return torch.stack(scale)
+        data = ((data * target_sd) + target_mean)
     return data
 
 
@@ -78,91 +68,183 @@ def train_model(
     log_epoch = Logger("results/epoch-log.txt")
     log("Model: %s" % model)
 
-    model.train()
-
     since = time.time()
     log_epoch("-" * 50)
     print("Training Initiated!")
     log_epoch("%s Training Initiated!" % time.asctime())
-    log_epoch("-" * 50)
+    log_epoch("")
+
     best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = 1e8
 
-    plot_loss_y = []
+    plot_loss_y = {"train": [], "val": []}
 
     epoch = 0
     while best_loss >= rmse_criteria:
+    # while epoch <= 30:
         log_epoch("{} Epoch {}".format(time.asctime(), epoch + 1))
-        log_epoch("-" * 40)
+        log_epoch("-" * 30)
 
-        mse = 0.0
+        if isinstance(atoms_dataloader, dict):
 
-        for data_sample in atoms_dataloader:
-            input_data = [data_sample[0], len(data_sample[1])]
-            fp_primes = data_sample[3]
-            image_forces = data_sample[4].type(torch.FloatTensor)
-            target = data_sample[1].requires_grad_(False)
-            batch_size = len(target)
-            target = target.reshape(batch_size, 1).to(device)
-            scaled_target = target_scaling(target, method="standardize")
-            scaled_forces = target_scaling(image_forces, method='standardize')
-            print(scaled_forces)
-            sys.exit()
-            num_of_atoms = data_sample[2].reshape(batch_size, 1).to(device)
-            # Send inputs and labels to the corresponding device (cpu or gpu)
-            for element in unique_atoms:
-                input_data[0][element][0] = (
-                    input_data[0][element][0].to(device).requires_grad_(True)
+            for phase in ["train", "val"]:
+
+                if phase == "train":
+                    if scheduler:
+                        scheduler.step()
+                    model.train()
+                else:
+                    model.eval()
+
+                mse = 0.0
+
+                for data_sample in atoms_dataloader[phase]:
+                    input_data = [data_sample[0], len(data_sample[1])]
+                    fp_primes = data_sample[3]
+                    image_forces = data_sample[4].to(device)
+                    target = data_sample[1].requires_grad_(False)
+                    batch_size = len(target)
+                    target = target.reshape(batch_size, 1).to(device)
+                    scaled_target = target_scaling(target, method="standardize")
+                    num_of_atoms = data_sample[2].reshape(batch_size, 1).to(device)
+                    for element in unique_atoms:
+                        input_data[0][element][0] = (
+                            input_data[0][element][0].to(device).requires_grad_(True)
+                        )
+                    scaled_target = scaled_target.to(device)
+
+                    def closure():
+                        optimizer.zero_grad()
+                        energy_pred, force_pred = model(input_data, fp_primes)
+                        CustomLoss = ForceLossFunction()
+                        loss = CustomLoss(
+                            energy_pred,
+                            scaled_target,
+                            force_pred,
+                            image_forces,
+                            num_of_atoms,
+                        )
+                        loss.backward()
+                        return loss
+
+                    energy_pred, force_pred = model(input_data, fp_primes)
+                    raw_preds = pred_scaling(energy_pred, target, method="standardize")
+                    raw_preds_per_atom = torch.div(raw_preds, num_of_atoms)
+                    target_per_atom = torch.div(target, num_of_atoms)
+                    MSELoss = nn.MSELoss()
+                    actual_loss = MSELoss(raw_preds_per_atom, target_per_atom)
+
+                    if phase == "train":
+                        optimizer.step(closure)
+
+                    mse += actual_loss.item()
+
+                mse = mse / dataset_size[phase]
+                rmse = np.sqrt(mse)
+                epoch_loss = rmse
+                plot_loss_y[phase].append(np.log10(rmse))
+
+                print("{} Loss: {:.4f}".format(phase, epoch_loss))
+
+                log_epoch(
+                    "{} {} Loss: {:.4f}".format(time.asctime(), phase, epoch_loss)
                 )
-            scaled_target = scaled_target.to(device)
-            # print(image_forces.requires_grad)
-            # print(scaled_target.requires_grad)
 
-            def closure():
-                """Allows optimizer to reevaluate the function multiple times
-                as per the LBFGS algorithm"""
-                optimizer.zero_grad()
+                if phase == "val" and epoch_loss < best_loss:
+                    best_loss = epoch_loss
+                    best_model_wts = copy.deepcopy(model.state_dict())
+
+                log_epoch("")
+
+        else:
+            phase = "train"
+
+            if scheduler:
+                scheduler.step()
+            model.train()
+
+            mse = 0.0
+            mse_f = 0.0
+
+            for data_sample in atoms_dataloader:
+                input_data = [data_sample[0], len(data_sample[1])]
+                fp_primes = data_sample[3]
+                image_forces = data_sample[4].to(device)
+                target = data_sample[1].requires_grad_(False)
+                batch_size = len(target)
+                target = target.reshape(batch_size, 1).to(device)
+                scaled_target = target_scaling(target, method="standardize")
+                scaled_forces = target_scaling(image_forces,
+                                               method="standardize")
+                num_of_atoms = data_sample[2].reshape(batch_size, 1).to(device)
+                for element in unique_atoms:
+                    input_data[0][element][0] = (
+                        input_data[0][element][0].to(device).requires_grad_(True)
+                    )
+                scaled_target = scaled_target.to(device)
+
+                def closure():
+                    optimizer.zero_grad()
+                    energy_pred, force_pred = model(input_data, fp_primes)
+                    # loss = criterion(energy_pred, scaled_target)
+                    # loss = criterion(force_pred, image_forces)
+                    CustomLoss = ForceLossFunction()
+                    loss = CustomLoss(
+                        energy_pred,
+                        scaled_target,
+                        force_pred,
+                        # scaled_forces,
+                        image_forces,
+                        num_of_atoms,
+                    )
+                    loss.backward()
+                    return loss
+
                 energy_pred, force_pred = model(input_data, fp_primes)
-                # print(energy_pred)
-                # loss = criterion(energy_pred, scaled_target)
-                # loss = criterion(force_pred, image_forces)
-                # loss.backward()
-                test = ForceLossFunction()
-                loss_test = test(
-                    energy_pred, scaled_target, force_pred, image_forces, num_of_atoms
+                raw_preds = pred_scaling(energy_pred, target, method="standardize")
+                raw_preds_per_atom = torch.div(raw_preds, num_of_atoms)
+                target_per_atom = torch.div(target, num_of_atoms)
+                actual_loss = criterion(raw_preds_per_atom, target_per_atom)
+
+                # force_pred = pred_scaling(force_pred, image_forces, method="standardize")
+                num_atoms_force = torch.cat(
+                    [idx.repeat(int(idx)) for idx in num_of_atoms]
                 )
-                loss_test.backward()
-                return loss_test
-                # return loss
+                num_atoms_force = torch.sqrt(
+                    num_atoms_force.reshape(len(num_atoms_force), 1)
+                )
+                force_pred_per_atom = torch.div(force_pred, num_atoms_force)
+                force_targets_per_atom = torch.div(image_forces, num_atoms_force)
+                force_loss = criterion(force_pred_per_atom, force_targets_per_atom)
 
-            # loss = optimizer.step(closure)
-            # Perform predictions and compute loss
-            # with torch.no_grad():
-            scaled_preds, force_pred = model(input_data, fp_primes)
-            raw_preds = pred_scaling(scaled_preds, target, method="standardize")
-            raw_preds_per_atom = torch.div(raw_preds, num_of_atoms)
-            target_per_atom = torch.div(target, num_of_atoms)
-            loss = criterion(raw_preds_per_atom, target_per_atom)
-            # loss = criterion(force_pred, image_forces)
-            mse += loss.item()
+                optimizer.step(closure)
 
-            optimizer.step(closure)
-            # loss = optimizer.step(closure)
-            # mse += loss.item()*batch_size
+                mse += actual_loss.item()
+                mse_f += force_loss.item()
 
-        mse = mse / dataset_size
-        rmse = np.sqrt(mse)
-        epoch_loss = rmse
-        print(epoch_loss)
-        plot_loss_y.append(np.log10(mse))
+            mse = mse / dataset_size
+            mse_f = mse_f / dataset_size
+            rmse = np.sqrt(mse)
+            rmse_f = np.sqrt(mse_f)
+            epoch_loss = rmse_f
+            print("energy: %s" % rmse)
+            print("force: %s" % rmse_f)
+            print("")
+            # print(epoch_loss)
+            plot_loss_y[phase].append(np.log10(rmse))
 
-        log_epoch("{} Loss: {:.4f}".format(time.asctime(), epoch_loss))
-        log_epoch("")
+            if epoch_loss < best_loss:
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
 
-        if epoch_loss < best_loss:
-            best_loss = epoch_loss
-            best_model_wts = copy.deepcopy(model.state_dict())
+            log_epoch("{} {} Loss: {:.4f}".format(time.asctime(), phase, epoch_loss))
+            log_epoch("")
+
         epoch += 1
+    # print(target)
+    # print(raw_preds)
+    # print(image_forces)
+    # print(force_pred)
 
     time_elapsed = time.time() - since
     print("Training complete in {} steps".format(epoch))
@@ -172,12 +254,6 @@ def train_model(
         )
     )
 
-    log("")
-    log(
-        "Training complete in {:.0f}m {:.0f}s".format(
-            time_elapsed // 60, time_elapsed % 60
-        )
-    )
     log("Training complete in {} steps".format(epoch))
     log("Best training loss: {:4f}".format(best_loss))
     log("")
@@ -186,7 +262,7 @@ def train_model(
     plt.xlabel("Epoch #")
     plt.ylabel("log(RMSE)")
     plot_epoch_x = list(range(1, epoch + 1))
-    plt.plot(plot_epoch_x, plot_loss_y, label="train")
+    plt.plot(plot_epoch_x, plot_loss_y[phase], label="train")
     plt.legend()
     plt.show()
     model.load_state_dict(best_model_wts)

examples/example.py
@@ -14,9 +14,9 @@ from amp_pytorch.NN_model import ForceLossFunction
 
 # locate training images
 IMAGES = "../datasets/water.extxyz"
-images = ase.io.read(IMAGES,":")
+images = ase.io.read(IMAGES, ":")
 IMAGES = []
-for i in range(10):
+for i in range(300):
     IMAGES.append(images[i])
 # IMAGES = [Atoms(symbols='PdOPd',
                         # pbc=np.array([True, False, False], dtype=bool),
@@ -55,9 +55,9 @@ DEVICE = "cpu"
 
 # NN architectures across different atoms are identical with the first index
 # representing the number of layers, and the second number representing the
-# number of nodes in each hidden layer. i.e. [3,3] = 3 layers (2 hidden layers,
-# 1 output layer) and 3 nodes in each hidden layer.
-ARCHITECTURE = [5, 5]
+# number of nodes in each hidden layer. i.e. [3,5] = 3 layers (2 hidden layers,
+# 1 output layer) and 5 nodes in each hidden layer.
+ARCHITECTURE = [3, 5]
 
 # define model
 MODEL = core.AMPtorch(IMAGES, DEVICE, batch_size=None, structure=ARCHITECTURE, val_frac=0)
@@ -66,11 +66,12 @@ MODEL = core.AMPtorch(IMAGES, DEVICE, batch_size=None, structure=ARCHITECTURE, v
 CRITERION = nn.MSELoss(reduction='sum')
 OPTIMIZER = optim.LBFGS
 # OPTIMIZER = optim.Adam
-RMSE_CRITERIA = 0.02
+RMSE_CRITERIA = 2e-2
 LR = 1
 
 # train the model
-TRAINED_MODEL = MODEL.train(CRITERION, OPTIMIZER,lr=LR, rmse_criteria=RMSE_CRITERIA)
+TRAINED_MODEL = MODEL.train(CRITERION, OPTIMIZER, lr=LR, rmse_criteria=RMSE_CRITERIA)
 # plotting
 MODEL.parity_plot(TRAINED_MODEL)
+MODEL.parity_plot_forces(TRAINED_MODEL)
 # MODEL.plot_residuals(TRAINED_MODEL)