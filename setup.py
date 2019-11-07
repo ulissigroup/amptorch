3994df9cc6e9c31da0a3631105cacc92cb656091
Muhammed <mshuaibi@andrew.cmu.edu>
2019-11-07 13:08:27 -0500

    contact info, gitignore

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='amptorch',
-      version='0.1',
-      description='A Pytorch Implementation of AMP',
-      author='Muhammed Shuaibi',
-      author_email='',
-      url='https://github.com/ulissigroup/amptorch',
-      packages=find_packages(),
-      install_requires=['spglib', 'torch','ase','scipy'],
-     )
-
+setup(
+    name="amptorch",
+    version="0.1",
+    description="A Pytorch Implementation of AMP",
+    author="Muhammed Shuaibi",
+    author_email="mshuaibi@andrew.cmu.edu",
+    url="https://github.com/ulissigroup/amptorch",
+    packages=find_packages(),
+    install_requires=["spglib", "torch", "ase", "scipy"],
+)
