97a1d793003e319fe738192dd925a2a5fb399eac
Muhammed <mshuaibi@andrew.cmu.edu>
2019-11-07 13:06:22 -0500

    md_work updates
    Former-commit-id: 7ef9feefb61112e244a8a52596bf2bba3f3bf1b5

--- examples/md_work/plots.py
+++ examples/md_work/plots.py
@@ -12,7 +12,7 @@ import seaborn as sns
 from ase.visualize import view
 
 
-def kde_plots(emt, forces, filename=None):
+def kde_plots(emt, forces, labels, filename=None):
     fig, ax = plt.subplots(figsize=(14.15, 10))
     sns.distplot(
         emt,
@@ -28,7 +28,7 @@ def kde_plots(emt, forces, filename=None):
         sns.distplot(
             data,
             hist=False,
-            label="iter %s" % (i + 1),
+            label=labels[i],
             kde=True,
             hist_kws={"edgecolor": "black", "alpha": 0.1},
             kde_kws={"linewidth": 2},
@@ -42,13 +42,14 @@ def kde_plots(emt, forces, filename=None):
     plt.yticks(fontsize=25)
     plt.show()
 
+
 def rmse_plots(emt, forces, interval, filename=None):
     fig, ax = plt.subplots(figsize=(14.15, 10))
     rmse = []
-    resample_images = [5*(i+1) for i in range(len(forces))]
+    resample_images = [interval * (i + 1) for i in range(len(forces))]
     for file_forces in forces:
         num_atoms = file_forces.shape[1]
-        mse = (1/(3*num_atoms))*((emt-file_forces)**2).sum()
+        mse = (1 / (3 * num_atoms)) * ((emt - file_forces) ** 2).sum()
         rmse.append(np.sqrt(mse))
     plt.plot(resample_images, rmse)
     plt.xlabel("Images resampled", fontsize=25)
@@ -58,43 +59,83 @@ def rmse_plots(emt, forces, interval, filename=None):
     plt.yticks(fontsize=20)
     plt.show()
 
-def compute_forces(dir, files, type='max'):
+
+def time_plots(target, preds, sampled_points, label, property="energy", filename=None):
+    fig, ax = plt.subplots(figsize=(14.15, 10))
+    time = np.linspace(0, 20 * len(target), 101)
+    sampled_time = [time[i] for i in sampled_points]
+    samples = [preds[0][i] for i in sampled_points]
+    plt.plot(time, target, color="k", label='target')
+    plt.plot(time, preds[0], label=label)
+    plt.plot(time, preds[1], label=label+" resample")
+    plt.plot(sampled_time, samples, 'o', label='sampled points')
+    plt.xlabel("time, (ps)", fontsize=25)
+    if property == "energy":
+        plt.ylabel("energy, eV", fontsize=25)
+    else:
+        plt.ylabel("logmax|F|", fontsize=25)
+    plt.legend(fontsize=25)
+    plt.xticks(fontsize=20)
+    # plt.ylim(ymin=8.5, ymax=9)
+    plt.yticks(fontsize=20)
+    plt.show()
+
+
+def compute_forces(dir, files, type="max"):
     forces = []
     for file in files:
-        images = ase.io.read(dir+file, ":")
+        images = ase.io.read(dir + file, ":")
         data = []
         for image in images:
             image.set_calculator(EMT())
-            if type=='max':
+            if type == "max":
                 data.append(np.log10(np.amax(np.abs(image.get_forces()))))
             else:
                 data.append(image.get_forces())
-        if type=='max':
+        if type == "max":
             forces.append((np.array(data)))
         else:
-            forces.append(np.array(data))
+            forces.append(data)
     return forces
 
+def compute_energies(dir, files):
+    energies = []
+    for file in files:
+        images = ase.io.read(dir + file, ":")
+        data = []
+        for image in images:
+            image.set_calculator(EMT())
+            data.append(image.get_potential_energy())
+        energies.append(data)
+    return energies
 
-    forces = []
 
 images_emt = ase.io.read("../../datasets/COCu/COCu_pbc_300K.traj", ":")
 images0 = []
 forces0 = []
 for i in range(101):
     images0.append(images_emt[i])
-    # forces0.append(images0[i].get_forces())
     forces0.append(np.log10((np.array(np.amax(np.abs(images_emt[i].get_forces()))))))
-forces0 = np.array(forces0)
+    # forces0.append(images_emt[i].get_forces())
+energies0 = [image.get_potential_energy() for image in images0]
 
 files = [
-    "MLMD_COCu_pbc_300K_l2amp_6SF-1.traj",
-    "MLMD_COCu_pbc_300K_l2amp_6SF-LJ-1.traj",
-    "MLMD_COCu_pbc_300K_l2amp_6SF-2.traj",
-    "MLMD_COCu_pbc_300K_l2amp_6SF-LJ-2.traj"
+    "MLMD_COCu_pbc_300K_tanh-LJ-1.traj",
+    # "MLMD_COCu_pbc_300K_l2amp_fixed_10_resample_2.traj",
+    # "MLMD_COCu_pbc_300K_l2amp_fixed_LJ_10_resample_2.traj",
+    "MLMD_COCu_pbc_300K_tanh2_LJ_10_resample_2.traj",
+    # "MLMD_COCu_pbc_300K_tanh_fixed_10_resample_2.traj",
+    # "MLMD_COCu_pbc_300K_tanh_fixed_LJ_10_resample_2.traj",
 ]
 
-forces = compute_forces(dir="MD_results/",
-        files=files, type='max')
-# rmse_plots(forces0, forces, 5, "test")
-kde_plots(forces0, forces, "test")
+forces = compute_forces(
+    dir="MD_results/COCu/pbc_300K/tanh/paper/", files=files, type="max"
+)
+# sampled_points = [10, 12, 14, 16, 18, 20, 22, 24, 26, 30]
+# sampled_points = [73, 63, 3, 12, 23, 68, 16, 97, 89, 19]
+sampled_points = [79, 77, 35, 17, 70, 41, 9, 66, 28, 75]
+energies = compute_energies(dir="MD_results/COCu/pbc_300K/tanh/paper/", files=files)
+time_plots(forces0, forces, sampled_points, label='ML-LJ', property="forces", filename="test")
+time_plots(energies0, energies, sampled_points, label='ML-LJ', property="energy", filename="test")
+# rmse_plots(forces0, forces[0], 10, "test")
+# kde_plots(forces0, forces, ['ML', 'ML-LJ'], "test")
