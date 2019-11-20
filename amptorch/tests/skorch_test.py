57871d8a5131075456d8cd044eca862933435a43
Muhammed <mshuaibi@andrew.cmu.edu>
2019-11-20 11:25:43 -0500

    skorch test cleanup

--- amptorch/tests/skorch_test.py
+++ amptorch/tests/skorch_test.py
@@ -10,6 +10,7 @@ from skorch.callbacks.lr_scheduler import LRScheduler
 import skorch.callbacks.base
 from amptorch.gaussian import Gaussian
 from amptorch.skorch import AMP
+from amptorch.skorch.utils import target_extractor, energy_score, forces_score
 from amptorch.model import FullNN, CustomLoss
 from amptorch.data_preprocess import (
     AtomsDataset,
@@ -25,52 +26,6 @@ from sklearn.pipeline import Pipeline
 from ase import Atoms
 from ase.calculators.emt import EMT
 from ase.io import read
-
-
-def target_extractor(y):
-    return (
-        (to_numpy(y[0]), to_numpy(y[1]))
-        if len(y) == 2
-        else (to_numpy(y[0]), to_numpy(y[1]), to_numpy(y[2]))
-    )
-
-
-def energy_score(net, X, y):
-    mse_loss = MSELoss(reduction="sum")
-    energy_pred = net.infer()[0]
-    device = energy_pred.device
-    energy_target = torch.tensor(y[0]).to(device)
-    num_atoms = torch.tensor(y[1]).to(device)
-    dataset_size = len(energy_pred)
-    sd_scaling = float(X[0][-1][0])
-    mean_scaling = float(X[0][-1][1])
-    raw_preds = (energy_pred * sd_scaling) + mean_scaling
-    raw_preds_per_atom = torch.div(raw_preds, num_atoms)
-    raw_targets = (energy_target * sd_scaling) + mean_scaling
-    target_per_atom = torch.div(raw_targets, num_atoms)
-    energy_loss = mse_loss(raw_preds_per_atom, target_per_atom)
-    energy_loss /= dataset_size
-    energy_rmse = torch.sqrt(energy_loss)
-    return energy_rmse
-
-
-def forces_score(net, X, y):
-    mse_loss = MSELoss(reduction="sum")
-    sd_scaling = float(X[0][-1][0])
-    force_pred = net.infer()[1] * sd_scaling
-    device = force_pred.device
-    num_atoms = torch.tensor(y[1]).to(device)
-    force_target = torch.tensor(y[-1], device=device)
-    dataset_size = len(num_atoms)
-    raw_force_target = force_target * sd_scaling
-    num_atoms_force = torch.cat([idx.repeat(int(idx)) for idx in num_atoms])
-    num_atoms_force = torch.sqrt(num_atoms_force).reshape(len(num_atoms_force), 1)
-    force_pred_per_atom = torch.div(force_pred, num_atoms_force)
-    force_targets_per_atom = torch.div(raw_force_target, num_atoms_force)
-    force_mse = mse_loss(force_pred_per_atom, force_targets_per_atom)
-    force_mse /= 3 * dataset_size
-    force_rmse = torch.sqrt(force_mse)
-    return force_rmse
 
 
 class train_end_load_best_valid_loss(skorch.callbacks.base.Callback):
