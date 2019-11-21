b34a7e472bb8a3aca84f1b3a5267545c0c621727
Muhammed <mshuaibi@andrew.cmu.edu>
2019-11-21 18:01:24 -0500

    validation scoring bug resolved

--- amptorch/skorch/utils.py
+++ amptorch/skorch/utils.py
@@ -1,3 +1,4 @@
+import sys
 from skorch.utils import to_numpy
 import torch
 from torch.nn import MSELoss
@@ -12,16 +13,18 @@ def target_extractor(y):
 
 def energy_score(net, X, y):
     mse_loss = MSELoss(reduction="sum")
-    if hasattr(X, 'scalings'):
-        sd_scaling = X.scalings[0]
-        mean_scaling = X.scalings[1]
-    else:
-        sd_scaling = X.dataset.scalings[0]
-        mean_scaling = X.dataset.scalings[1]
     energy_pred, _ = net.forward(X)
     device = energy_pred.device
-    energy_target = X.energy_dataset.to(device).reshape(-1, 1)
-    num_atoms = torch.FloatTensor(X.num_of_atoms).reshape(-1, 1).to(device)
+    if not hasattr(X, 'scalings'):
+        indices = X.indices
+        X = X.dataset
+        num_atoms = torch.FloatTensor(X.num_of_atoms[indices]).reshape(-1, 1).to(device)
+        energy_target = X.energy_dataset[indices].to(device).reshape(-1, 1)
+    else:
+        num_atoms = torch.FloatTensor(X.num_of_atoms).reshape(-1, 1).to(device)
+        energy_target = X.energy_dataset.to(device).reshape(-1, 1)
+    sd_scaling = X.scalings[0]
+    mean_scaling = X.scalings[1]
     dataset_size = len(energy_pred)
     raw_preds = (energy_pred * sd_scaling) + mean_scaling
     raw_preds_per_atom = torch.div(raw_preds, num_atoms)
@@ -34,15 +37,19 @@ def energy_score(net, X, y):
 
 def forces_score(net, X, y):
     mse_loss = MSELoss(reduction="sum")
-    if hasattr(X, 'scalings'):
-        sd_scaling = X.scalings[0]
-    else:
-        sd_scaling = X.dataset.scalings[0]
     _, forces = net.forward(X)
+    device = forces.device
+    if not hasattr(X, 'scalings'):
+        indices = X.indices
+        X = X.dataset
+        num_atoms = torch.FloatTensor(X.num_of_atoms[indices]).reshape(-1, 1).to(device)
+        force_target = torch.cat([X.forces_dataset[idx] for idx in indices]).float().to(device)
+    else:
+        num_atoms = torch.FloatTensor(X.num_of_atoms).reshape(-1, 1).to(device)
+        force_target = torch.cat(X.forces_dataset).float().to(device)
+    sd_scaling = X.scalings[0]
     force_pred = forces * sd_scaling
     device = force_pred.device
-    num_atoms = torch.FloatTensor(X.num_of_atoms).reshape(-1, 1).to(device)
-    force_target = torch.cat(X.forces_dataset).float().to(device)
     dataset_size = len(num_atoms)
     raw_force_target = force_target * sd_scaling
     num_atoms_force = torch.cat([idx.repeat(int(idx)) for idx in num_atoms])
