14975af9f4e70a3969d32a7eeff28c52f023ae6d
Muhammed <mshuaibi@andrew.cmu.edu>
2020-01-29 11:54:58 -0500

    critical: resolved forces bug associated with mismatched primes
    Former-commit-id: 834ad6d3447b76a327e42cc866511997463a3eb9

--- amptorch/model.py
+++ amptorch/model.py
@@ -125,6 +125,7 @@ class FullNN(nn.Module):
             input_data = inputs[0]
             batch_size = inputs[1]
             batch_elements = inputs[2]
+            rearange = inputs[-1]
             if self.device == 'cpu':
                 energy_pred = torch.zeros(batch_size, 1).to(self.device)
             else:
@@ -132,7 +133,7 @@ class FullNN(nn.Module):
             force_pred = torch.tensor([])
             # Constructs an Nx1 empty tensor to store element energy contributions
             if self.forcetraining:
-                fprimes = inputs[-1]
+                fprimes = inputs[-2]
                 dE_dFP = torch.tensor([]).to(self.device)
                 idx = torch.tensor([]).to(self.device)
             for index, element in enumerate(batch_elements):
@@ -153,7 +154,8 @@ class FullNN(nn.Module):
             if self.forcetraining:
                 boolean = idx[:, None] == torch.unique(idx)
                 ordered_idx = torch.nonzero(boolean.t())[:, -1]
-                dE_dFP = torch.index_select(dE_dFP, 0, ordered_idx).reshape(1, -1)
+                dE_dFP = torch.index_select(dE_dFP, 0, ordered_idx)
+                dE_dFP = torch.index_select(dE_dFP, 0, rearange).reshape(1, -1)
                 """Constructs a 1xPQ tensor that contains the derivatives with respect to
                 each atom's fingerprint"""
                 force_pred = -1 * torch.sparse.mm(fprimes.t(), dE_dFP.t())
@@ -165,7 +167,6 @@ class FullNN(nn.Module):
                 predictions in the same order and shape as the target forces calculated
                 from AMP."""
         return energy_pred, force_pred
-
 
 class CustomLoss(nn.Module):
     """Custom loss function to be optimized by the regression. Includes aotmic
@@ -210,12 +211,9 @@ class CustomLoss(nn.Module):
             loss = 0.5 * energy_loss
         return loss
 
-class TanhLoss(nn.Module):
-    """Custom loss function to be optimized by the regression. Includes aotmic
-    energy and force contributions."""
-
+class MAELoss(nn.Module):
     def __init__(self, force_coefficient=0):
-        super(TanhLoss, self).__init__()
+        super(MAELoss, self).__init__()
         self.alpha = force_coefficient
 
     def forward(
@@ -226,24 +224,59 @@ class TanhLoss(nn.Module):
         energy_pred = prediction[0]
         energy_targets = target[0]
         num_atoms = target[1]
-        MSE_loss = nn.MSELoss(reduction="sum")
+        MAE_loss = nn.L1Loss(reduction="sum")
         energy_per_atom = torch.div(energy_pred, num_atoms)
         targets_per_atom = torch.div(energy_targets, num_atoms)
-        energy_loss = MSE_loss(energy_per_atom, targets_per_atom)
-        # energy_loss = torch.sum(torch.tanh(torch.abs(energy_per_atom - targets_per_atom)))
+        energy_loss = MAE_loss(energy_per_atom, targets_per_atom)
 
         if self.alpha > 0:
             force_pred = prediction[1]
+            if force_pred.nelement() == 0:
+                raise Exception('Force training disabled. Set force_coefficient to 0')
             force_targets = target[-1]
             num_atoms_force = torch.cat([idx.repeat(int(idx)) for idx in num_atoms])
-            num_atoms_force = torch.sqrt(
-                num_atoms_force.reshape(len(num_atoms_force), 1)
-            )
+            num_atoms_force = num_atoms_force.reshape(len(num_atoms_force), 1)
             force_pred_per_atom = torch.div(force_pred, num_atoms_force)
             force_targets_per_atom = torch.div(force_targets, num_atoms_force)
-            force_loss = (1 / 3) * self.alpha * torch.sum(
-                torch.tanh(torch.abs(force_pred_per_atom - force_targets_per_atom))
+            force_loss = (self.alpha / 3) * MAE_loss(
+                force_pred_per_atom, force_targets_per_atom
             )
+            loss = 0.5 * (energy_loss + force_loss)
+        else:
+            loss = 0.5 * energy_loss
+        return loss
+
+class HuberLoss(nn.Module):
+    def __init__(self, force_coefficient=0):
+        super(HuberLoss, self).__init__()
+        self.alpha = force_coefficient
+
+    def forward(
+        self,
+        prediction,
+        target):
+
+        energy_pred = prediction[0]
+        energy_targets = target[0]
+        num_atoms = target[1]
+        huber_loss = nn.SmoothL1Loss(reduction="none")
+        energy_per_atom = torch.div(energy_pred, num_atoms)
+        targets_per_atom = torch.div(energy_targets, num_atoms)
+        energy_loss = huber_loss(energy_per_atom, targets_per_atom)
+        energy_loss = torch.sum(energy_loss)
+
+        if self.alpha > 0:
+            force_pred = prediction[1]
+            if force_pred.nelement() == 0:
+                raise Exception('Force training disabled. Set force_coefficient to 0')
+            force_targets = target[-1]
+            num_atoms_force = torch.cat([idx.repeat(int(idx)) for idx in num_atoms]).reshape(-1, 1)
+            force_pred_per_atom = torch.div(force_pred, num_atoms_force)
+            force_targets_per_atom = torch.div(force_targets, num_atoms_force)
+            force_loss = (self.alpha / 3) * huber_loss(
+                force_pred_per_atom, force_targets_per_atom
+            )
+            force_loss = torch.sum(force_loss)
             loss = energy_loss + force_loss
         else:
             loss = energy_loss
