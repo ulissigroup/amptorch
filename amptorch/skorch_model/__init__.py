9f12026772664d47f8c9b4d504841cdac41d2244
Muhammed <mshuaibi@andrew.cmu.edu>
2020-02-21 09:43:05 -0500

    added skorch training logs

--- amptorch/skorch_model/__init__.py
+++ amptorch/skorch_model/__init__.py
@@ -1,11 +1,18 @@
 """Base AMP calculator class to be utilized similiar to other ASE calculators"""
 
-import copy
-import sys
-import numpy as np
 import os
+import copy
+import numpy as np
+import time
 from torch.utils.data import DataLoader
 from amptorch.utils import Logger, hash_images
+from amptorch.skorch_model.utils import (
+    make_force_header,
+    make_energy_header,
+    make_val_force_header,
+    make_val_energy_header,
+    log_results,
+)
 from amptorch.data_preprocess import (
     AtomsDataset,
     factorize_data,
@@ -36,20 +43,18 @@ class AMP(Calculator):
 
     implemented_properties = ["energy", "forces"]
 
-    def __init__(self, training_data, model, label):
+    def __init__(self, training_data, model, label, save_logs=True):
         Calculator.__init__(self)
 
-        if not os.path.exists("results/trained_models"):
-            os.makedirs("results/trained_models")
+        os.makedirs("results/", exist_ok=True)
+        os.makedirs("results/trained_models", exist_ok=True)
+        os.makedirs("results/logs", exist_ok=True)
         self.model = model
         self.testlabel = label
         self.label = "".join(["results/trained_models/", label, ".pt"])
-        if training_data.scaling_scheme is not "log":
-            self.scalings = training_data.scalings
-            self.target_sd = self.scalings[0]
-            self.target_mean = self.scalings[1]
-        else:
-            self.scalings = "log"
+        self.scalings = training_data.scalings
+        self.target_slope = self.scalings[0]
+        self.target_intercept = self.scalings[1]
         self.lj = training_data.lj
         self.Gs = training_data.Gs
         self.fprange = training_data.fprange
@@ -61,8 +66,51 @@ class AMP(Calculator):
             self.params_dict = self.training_data.lj_data[4]
             self.lj_model = self.training_data.lj_data[5]
 
+        # TODO make utility logging function
+        self.log = Logger("results/logs/{}.txt".format(label))
+        if not self.lj:
+            self.log(time.asctime())
+            self.log("-" * 50)
+        self.log("Filename: {}".format(label))
+        self.log("Dataset size: {}".format(len(self.training_data)))
+        self.log("Target scaling: {}".format(self.scalings))
+        self.log("Symmetry function parameters:")
+        for i in self.Gs.keys():
+            self.log("     {}: {}".format(i, self.Gs[i]))
+        self.log("Device: {:s}".format(self.model.device))
+        self.log("Model: {}".format(model.module))
+        self.log(
+            "Architecture:\n   Input Layer - {}\n   # of Hidden Layers - {}\n   Nodes/Layer - {}".format(
+                self.model.module.architecture[0],
+                self.model.module.architecture[1] - 1,
+                self.model.module.architecture[2],
+            )
+        )
+        self.log("Loss Function: {}".format(self.model.criterion))
+        self.log(
+            "Force coefficient: {}".format(self.model.criterion__force_coefficient)
+        )
+        self.log("Optimizer: {}".format(self.model.optimizer))
+        self.log("Learning Rate: {}".format(self.model.lr))
+        self.log("Batch Size: {}".format(self.model.batch_size))
+        self.log("Epochs: {}".format(self.model.max_epochs))
+        self.log("Shuffle: {}".format(self.model.iterator_train__shuffle))
+        if self.model.train_split != 0:
+            self.log(
+                "Train Split (k-fold if int, fraction if float): {}\n".format(
+                    self.model.train_split.cv
+                )
+            )
+        else:
+            self.log(
+                "Train Split (k-fold if int, fraction if float): {}\n".format(
+                    self.model.train_split
+                )
+            )
+
     def train(self, overwrite=True):
         self.model.fit(self.training_data, None)
+        log_results(self.model, self.log)
         if os.path.exists(self.label):
             if overwrite is False:
                 print("Could not save! File already exists")
@@ -94,20 +142,10 @@ class AMP(Calculator):
         for inputs in dataloader:
             for element in unique_atoms:
                 inputs[0][element][0] = inputs[0][element][0].requires_grad_(True)
-            num_of_atoms = inputs[3]
             energy, forces = model(inputs)
-        if self.scalings is not "log":
-            energy = (energy * self.target_sd) + self.target_mean
-            energy = np.concatenate(energy.detach().numpy())
-            forces = (forces * self.target_sd).detach().numpy()
-        else:
-            energy = torch.exp(energy) - 1
-            energy_atoms = torch.cat(
-                [val.repeat(num_of_atoms[idx]) for idx, val in enumerate(energy)]
-            ).reshape(-1, 1)
-            energy = np.concatenate(energy.detach().numpy())
-            forces = forces * (energy_atoms + 1)
-            forces = forces.detach().numpy()
+        energy = (energy * self.target_slope) + self.target_intercept
+        energy = np.concatenate(energy.detach().numpy())
+        forces = (forces * self.target_slope).detach().numpy()
 
         if self.lj:
             image_hash = hash_images([atoms])
