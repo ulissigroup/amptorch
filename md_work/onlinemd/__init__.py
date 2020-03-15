25c47af2ce912debd8a43bb264518316d8c20a0a
Muhammed <mshuaibi@andrew.cmu.edu>
2020-03-15 11:11:45 -0400

    model parallelization issue resolved
    Former-commit-id: f7da30eba6d2c5181a3be84a5b18826a18a98492

--- md_work/onlinemd/__init__.py
+++ md_work/onlinemd/__init__.py
@@ -1,40 +1,18 @@
-"""Base AMP calculator class to be utilized similiar to other ASE calculators"""
-
 import os
-import copy
 import numpy as np
-import time
-from torch.utils.data import DataLoader
-from amptorch.utils import Logger, hash_images, get_hash
-from amptorch.skorch_model.utils import (
-    make_force_header,
-    make_energy_header,
-    make_val_force_header,
-    make_val_energy_header,
-    log_results,
-)
-from amptorch.data_preprocess import (
-    AtomsDataset,
-    factorize_data,
-    collate_amp,
-    TestDataset,
-)
+from amptorch.data_preprocess import AtomsDataset, collate_amp
 from amptorch.model import FullNN, CustomLoss
+import ase
 from ase.calculators.calculator import Calculator, Parameters
+from ase.md.nvtberendsen import NVTBerendsen
 import torch
+from torch.nn import init
 import multiprocessing
 import random
 from amptorch.utils import make_amp_descriptors_simple_nn
-import sys
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-from ase.md.verlet import VelocityVerlet
-from ase.md.nvtberendsen import NVTBerendsen
 from ase.calculators.emt import EMT
-from ase.calculators.singlepoint import SinglePointCalculator as sp
-from ase.md import Langevin
-from ase import units
 from amptorch.lj_model import lj_optim
-import ase
+import skorch
 from skorch import NeuralNetRegressor
 from skorch.dataset import CVSplit
 from skorch.callbacks import Checkpoint, EpochScoring
@@ -42,8 +20,6 @@ from skorch.callbacks.lr_scheduler import LRScheduler
 from amptorch.gaussian import SNN_Gaussian
 from amptorch.skorch_model import AMP
 from amptorch.skorch_model.utils import target_extractor, energy_score, forces_score
-from torch.nn import init
-from skorch.utils import to_numpy
 
 
 __author__ = "Muhammed Shuaibi"
@@ -66,17 +42,16 @@ class AMPOnlineCalc(Calculator):
 
     implemented_properties = ["energy", "forces"]
 
-    def __init__(self, parent_dataset, n_ensembles):
+    def __init__(self, parent_dataset, parent_calc, n_ensembles):
         Calculator.__init__(self)
 
         self.n_ensembles = n_ensembles
+        self.parent_calc = parent_calc
         self.ensemble_sets, self.parent_dataset = self.bootstrap_ensemble(
             parent_dataset, n_ensembles=n_ensembles
         )
         self.trained_calcs = self.construct_calc(train(self.ensemble_sets))
-        self.uncertain_tol = 0.01
-        # self.data_to_retrain = []
-        # self.train_trigger = 10
+        self.uncertain_tol = 0.001
 
     def bootstrap_ensemble(
         self, parent_dataset, resampled_set=None, new_data=None, n_ensembles=1
@@ -120,6 +95,8 @@ class AMPOnlineCalc(Calculator):
 
         energies = []
         forces = []
+
+        make_amp_descriptors_simple_nn([atoms], Gs, elements, cores=10, label="test")
         for calc in self.trained_calcs:
             energies.append(calc.get_potential_energy(atoms))
             forces.append(calc.get_forces(atoms))
@@ -127,69 +104,45 @@ class AMPOnlineCalc(Calculator):
         forces = np.array(forces)
         energy_pred, force_pred, uncertainty = self.calculate_stats(energies, forces)
 
-        # if uncertainty >= self.uncertain_tol:
-        if uncertainty != 0:
+        if uncertainty >= self.uncertain_tol:
             new_data = atoms.copy()
-            new_data.set_calculator(EMT())
-            self.results["energy"] = new_data.get_potential_energy(apply_constraint=False)
+            new_data.set_calculator(self.parent_calc)
+            self.results["energy"] = new_data.get_potential_energy(
+                apply_constraint=False
+            )
             self.results["forces"] = new_data.get_forces(apply_constraint=False)
             self.ensemble_sets, self.parent_dataset = self.bootstrap_ensemble(
                 self.parent_dataset, self.ensemble_sets, new_data=new_data
             )
             self.trained_calcs = self.construct_calc(train(self.ensemble_sets))
-            sys.exit()
         else:
             self.results["energy"] = float(energy_pred)
             self.results["forces"] = force_pred
 
-def train(ensemble_sets):
-    Gs = {}
-    Gs["G2_etas"] = np.logspace(np.log10(0.05), np.log10(5.0), num=4)
-    Gs["G2_rs_s"] = [0] * 4
-    Gs["G4_etas"] = [0.005]
-    Gs["G4_zetas"] = [1.0, 4.0]
-    Gs["G4_gammas"] = [+1.0, -1]
-    Gs["cutoff"] = 5.876798323827276  # EMT asap_cutoff: False
 
+def train(ensemble_sets):
     pool = multiprocessing.Pool(len(ensemble_sets))
 
     input_data = []
-    # results = []
     for _ in range(len(ensemble_sets)):
         inputs = [ensemble_sets[_], "test" + str(_), "./", Gs, True, True, None]
-        # results.append(train_calc(inputs))
-        # inputs = [ensemble_sets[_], "test" + str(_), "./", Gs, True, True, None]
         input_data.append(inputs)
     results = pool.map(train_calc, input_data)
     return results
 
+
 def train_calc(inputs):
     images, filename, file_dir, Gs, lj, forcesonly, scaling = inputs
+    class train_end_load_best_valid_loss(skorch.callbacks.base.Callback):
+            def on_train_end(self, net, X, y):
+                net.load_params("./results/checkpoints/{}_params.pt".format(filename))
+    cp = Checkpoint(
+            monitor="forces_score_best",
+            fn_prefix="./results/checkpoints/{}_".format(filename),
+        )
+
     if not os.path.exists(file_dir):
         os.makedirs(file_dir, exist_ok=True)
-    # lj optimization
-    cutoff = Gs["cutoff"]
-    lj_data = None
-    if lj:
-        a = 12
-        p0 = [1.0, 6.3535, 0, 1.0808, 8.5357, 0, 2.1717, 3.7575, 0, a]
-        params_dict = {"C": [], "O": [], "Cu": []}
-        lj_model = lj_optim(
-            images, p0, params_dict, cutoff, filename, forcesonly=forcesonly
-        )
-        fitted_params = p0
-        # fitted_params = lj_model.fit()
-        lj_energies, lj_forces, num_atoms = lj_model.lj_pred(
-            images, fitted_params, params_dict
-        )
-        lj_data = [
-            lj_energies,
-            lj_forces,
-            num_atoms,
-            fitted_params,
-            params_dict,
-            lj_model,
-        ]
 
     forcetraining = True
     training_data = AtomsDataset(
@@ -199,7 +152,7 @@ def train_calc(inputs):
         forcetraining=forcetraining,
         label=filename,
         cores=1,
-        lj_data=lj_data,
+        lj_data=None,
         scaling=scaling,
     )
     unique_atoms = training_data.elements
@@ -217,9 +170,9 @@ def train_calc(inputs):
         optimizer=torch.optim.LBFGS,
         lr=1e-1,
         batch_size=len(training_data),
-        max_epochs=3,
+        max_epochs=200,
         iterator_train__collate_fn=collate_amp,
-        iterator_train__shuffle=True,
+        iterator_train__shuffle=False,
         iterator_valid__collate_fn=collate_amp,
         iterator_valid__shuffle=False,
         device=device,
@@ -243,21 +196,8 @@ def train_calc(inputs):
     calc.train()
     return [training_data, net, filename]
 
-Gs = {}
-Gs["G2_etas"] = np.logspace(np.log10(0.05), np.log10(5.0), num=4)
-Gs["G2_rs_s"] = [0] * 4
-Gs["G4_etas"] = [0.005]
-Gs["G4_zetas"] = [1.0, 4.0]
-Gs["G4_gammas"] = [+1.0, -1]
-Gs["cutoff"] = 5.876798323827276  # EMT asap_cutoff: False
-images = ase.io.read("../../datasets/COCu_ber_100ps_300K.traj", ":10")
-elements = np.array([atom.symbol for atoms in images for atom in atoms])
-_, idx = np.unique(elements, return_index=True)
-elements = list(elements[np.sort(idx)])
-make_amp_descriptors_simple_nn(images, Gs, elements, cores=10, label="test")
 
-
-def md_run2(
+def md_run(
     parent_dataset,
     dft_calculator,
     starting_image,
@@ -266,20 +206,30 @@ def md_run2(
     count,
     label,
     ensemble="NVE",
-    new_data=None,
 ):
     slab = starting_image.copy()
-    slab.set_calculator(AMPOnlineCalc(parent_dataset, 3))
+    slab.set_calculator(AMPOnlineCalc(parent_dataset, dft_calculator, 3))
     # MaxwellBoltzmannDistribution(slab, temp * units.kB)
-    if ensemble == "NVE":
-        dyn = VelocityVerlet(slab, dt * units.fs)
-    elif ensemble == "nvtberendsen":
-        dyn = NVTBerendsen(slab, dt * units.fs, temp, taut=300 * units.fs)
-    elif ensemble == "langevin":
-        dyn = Langevin(slab, dt * units.fs, temp * units.kB, 0.002)
+    if ensemble == "nvtberendsen":
+        dyn = NVTBerendsen(slab, dt * ase.units.fs, temp, taut=300 * ase.units.fs)
     traj = ase.io.Trajectory(label + ".traj", "w", slab)
     dyn.attach(traj.write, interval=1)
     dyn.run(count - 1)
 
 
-md_run2(images, EMT(), images[0], 300, 1, 5, "test", "nvtberendsen")
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
+    Gs = {}
+    Gs["G2_etas"] = np.logspace(np.log10(0.05), np.log10(5.0), num=4)
+    Gs["G2_rs_s"] = [0] * 4
+    Gs["G4_etas"] = [0.005]
+    Gs["G4_zetas"] = [1.0, 4.0]
+    Gs["G4_gammas"] = [+1.0, -1]
+    Gs["cutoff"] = 5.876798323827276  # EMT asap_cutoff: False
+    images = ase.io.read("../../datasets/COCu_ber_100ps_300K.traj", ":100")
+    elements = np.array([atom.symbol for atoms in images for atom in atoms])
+    _, idx = np.unique(elements, return_index=True)
+    elements = list(elements[np.sort(idx)])
+    make_amp_descriptors_simple_nn(images, Gs, elements, cores=10, label="test")
+
+    md_run(images, EMT(), images[0], 300, 1, 2000, "test", "nvtberendsen")
