e2d9767f7fdf549a1cec33e50592a5e16a77652d
mshuaibi_home <mshuaibi@andrew.cmu.edu>
2020-04-07 16:13:57 -0500

    updated oal calculator
    Former-commit-id: dd9c671166330fc874ece8a695e42fa666338d19

--- amptorch/active_learning/oal_calc.py
+++ amptorch/active_learning/oal_calc.py
@@ -1,25 +1,35 @@
+import sys
 import os
 import numpy as np
-from amptorch.data_preprocess import AtomsDataset, collate_amp
-from amptorch.model import FullNN, CustomMSELoss
-import ase
-from ase.calculators.calculator import Calculator, Parameters
-from ase.md.nvtberendsen import NVTBerendsen
-import torch
-from torch.nn import init
 import multiprocessing
 import random
-from amptorch.utils import make_amp_descriptors_simple_nn
+
+import torch
+from torch.nn import init
+
+import ase
 from ase.calculators.emt import EMT
-from amptorch.lj_model import lj_optim
+from ase.calculators.singlepoint import SinglePointCalculator as sp
+from ase.calculators.calculator import Calculator, Parameters
+from ase.md.nvtberendsen import NVTBerendsen
+from ase.build import fcc100, add_adsorbate, molecule
+from ase.constraints import FixAtoms
+from ase.optimize import BFGS, QuasiNewton
+
 import skorch
 from skorch import NeuralNetRegressor
 from skorch.dataset import CVSplit
 from skorch.callbacks import Checkpoint, EpochScoring
 from skorch.callbacks.lr_scheduler import LRScheduler
+
 from amptorch.gaussian import SNN_Gaussian
 from amptorch.skorch_model import AMP
 from amptorch.skorch_model.utils import target_extractor, energy_score, forces_score
+from amptorch.delta_models.morse import morse_potential
+from amptorch.utils import make_amp_descriptors_simple_nn
+from amptorch.data_preprocess import AtomsDataset, collate_amp
+from amptorch.model import FullNN, CustomMSELoss
+from amptorch.active_learning.generator_funcs import MDsimulate, Relaxation
 
 
 __author__ = "Muhammed Shuaibi"
@@ -52,12 +62,13 @@ class AMPOnlineCalc(Calculator):
         )
         self.trained_calcs = self.construct_calc(train(self.ensemble_sets))
         self.uncertain_tol = 0.001
+        self.dft_calls = 0
 
     def bootstrap_ensemble(
         self, parent_dataset, resampled_set=None, new_data=None, n_ensembles=1
     ):
-        if len(parent_dataset) == 1:
-            return [parent_dataset] * n_ensembles
+        if len(parent_dataset) == 1 and new_data is None :
+            return [parent_dataset.copy()] * n_ensembles, parent_dataset
         ensemble_sets = []
         if new_data is not None and resampled_set is not None:
             n_ensembles = len(resampled_set)
@@ -88,6 +99,7 @@ class AMPOnlineCalc(Calculator):
         energy_mean = np.mean(energies)
         energy_var = np.var(energies)
         forces_mean = np.mean(forces, axis=0)
+        forces_var = np.var(forces, axis=0)
         return energy_mean, forces_mean, energy_var
 
     def calculate(self, atoms, properties, system_changes):
@@ -104,20 +116,30 @@ class AMPOnlineCalc(Calculator):
         forces = np.array(forces)
         energy_pred, force_pred, uncertainty = self.calculate_stats(energies, forces)
 
-        if uncertainty >= self.uncertain_tol:
+        # if uncertainty == self.uncertain_tol:
+        if uncertainty >= 0.001:
             new_data = atoms.copy()
             new_data.set_calculator(self.parent_calc)
-            self.results["energy"] = new_data.get_potential_energy(
-                apply_constraint=False
-            )
-            self.results["forces"] = new_data.get_forces(apply_constraint=False)
+
+            sample_energy = new_data.get_potential_energy(apply_constraint=False)
+            sample_forces = new_data.get_forces(apply_constraint=False)
+            new_data.set_calculator(sp(atoms=new_data, energy=sample_energy,
+                forces=sample_forces))
+
             self.ensemble_sets, self.parent_dataset = self.bootstrap_ensemble(
                 self.parent_dataset, self.ensemble_sets, new_data=new_data
             )
+
             self.trained_calcs = self.construct_calc(train(self.ensemble_sets))
+            self.dft_calls += 1
+
+            self.results["energy"] = sample_energy
+            self.results["forces"] = sample_forces
+
         else:
-            self.results["energy"] = float(energy_pred)
+            self.results["energy"] = energy_pred
             self.results["forces"] = force_pred
+            print('step')
 
 
 def train(ensemble_sets):
@@ -132,7 +154,7 @@ def train(ensemble_sets):
 
 
 def train_calc(inputs):
-    images, filename, file_dir, Gs, lj, forcesonly, scaling = inputs
+    images, filename, file_dir, Gs, delta, forcesonly, scaling = inputs
     class train_end_load_best_valid_loss(skorch.callbacks.base.Callback):
             def on_train_end(self, net, X, y):
                 net.load_params("./results/checkpoints/{}_params.pt".format(filename))
@@ -152,8 +174,7 @@ def train_calc(inputs):
         forcetraining=forcetraining,
         label=filename,
         cores=1,
-        lj_data=None,
-        scaling=scaling,
+        delta_data=None,
     )
     unique_atoms = training_data.elements
     fp_length = training_data.fp_length
@@ -170,23 +191,25 @@ def train_calc(inputs):
         optimizer=torch.optim.LBFGS,
         lr=1e-1,
         batch_size=len(training_data),
-        max_epochs=200,
+        max_epochs=100,
         iterator_train__collate_fn=collate_amp,
         iterator_train__shuffle=False,
         iterator_valid__collate_fn=collate_amp,
         iterator_valid__shuffle=False,
         device=device,
-        train_split=CVSplit(cv=5, random_state=1),
+        verbose=0,
+        # train_split=CVSplit(cv=5, random_state=1),
+        train_split=0,
         callbacks=[
             EpochScoring(
                 forces_score,
-                on_train=False,
+                on_train=True,
                 use_caching=True,
                 target_extractor=target_extractor,
             ),
             EpochScoring(
                 energy_score,
-                on_train=False,
+                on_train=True,
                 use_caching=True,
                 target_extractor=target_extractor,
             ),
@@ -196,29 +219,9 @@ def train_calc(inputs):
     calc.train()
     return [training_data, net, filename]
 
-
-def md_run(
-    parent_dataset,
-    dft_calculator,
-    starting_image,
-    temp,
-    dt,
-    count,
-    label,
-    ensemble="NVE",
-):
-    slab = starting_image.copy()
-    slab.set_calculator(AMPOnlineCalc(parent_dataset, dft_calculator, 3))
-    # MaxwellBoltzmannDistribution(slab, temp * units.kB)
-    if ensemble == "nvtberendsen":
-        dyn = NVTBerendsen(slab, dt * ase.units.fs, temp, taut=300 * ase.units.fs)
-    traj = ase.io.Trajectory(label + ".traj", "w", slab)
-    dyn.attach(traj.write, interval=1)
-    dyn.run(count - 1)
-
-
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn")
+
     Gs = {}
     Gs["G2_etas"] = np.logspace(np.log10(0.05), np.log10(5.0), num=4)
     Gs["G2_rs_s"] = [0] * 4
@@ -226,10 +229,26 @@ if __name__ == "__main__":
     Gs["G4_zetas"] = [1.0, 4.0]
     Gs["G4_gammas"] = [+1.0, -1]
     Gs["cutoff"] = 5.876798323827276  # EMT asap_cutoff: False
-    images = ase.io.read("../../datasets/COCu_ber_100ps_300K.traj", ":100")
+
+    slab = fcc100("Cu", size=(3, 3, 3))
+    adsorbate = molecule("C")
+    add_adsorbate(slab, adsorbate, 3, offset=(1,1))
+    constraints = FixAtoms(
+            indices=[atom.index for atom in slab if (atom.tag == 3)])
+    slab.set_constraint(constraints)
+    slab.center(vacuum=13.0, axis=2)
+    slab.set_pbc(True)
+    slab.wrap(pbc=[True] * 3)
+    slab.set_calculator(EMT())
+
+    images = [slab]
+
     elements = np.array([atom.symbol for atoms in images for atom in atoms])
     _, idx = np.unique(elements, return_index=True)
     elements = list(elements[np.sort(idx)])
     make_amp_descriptors_simple_nn(images, Gs, elements, cores=10, label="test")
 
-    md_run(images, EMT(), images[0], 300, 1, 2000, "test", "nvtberendsen")
+    structure_optim = Relaxation(slab, BFGS, fmax=0.05, steps=None)
+    online_calc = AMPOnlineCalc(images, EMT(), 3)
+    structure_optim.run(online_calc, filename='relax_oal')
+    print(online_calc.dft_calls)
