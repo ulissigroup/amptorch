ee5d5b74ff6dbc6d17d612c4a6821d274d377795
Muhammed <mshuaibi@andrew.cmu.edu>
2020-04-20 14:54:14 -0400

    warning for # samples larger than available candidates
    Former-commit-id: c87aafdf2ba582b590d4ce7d1ce9c4bc595c29af

--- amptorch/active_learning/query_methods.py
+++ amptorch/active_learning/query_methods.py
@@ -2,39 +2,56 @@ import numpy as np
 from ase.calculators.singlepoint import SinglePointCalculator as sp
 import random
 import copy
+import warnings
 
-"""All query methods should have the following arguments
-        Paramaters
-        ----------
-        images: List. Current training images.
+"""
+All query methods should have the following arguments
+    Paramaters
+    ----------
+    images: List. Current training images.
 
-        sample_candidates: List. Potential candidates for query as collected
-        from the generating function.
+    sample_candidates: List. Potential candidates for query as collected
+    from the generating function.
 
-        samples_to_retrain: Integer. Number of samples to be randomly selected
-        for query and added to the training set.
+    samples_to_retrain: Integer. Number of samples to be randomly selected
+    for query and added to the training set.
 
-        parent_calc: Object. Parent calculator used to query energy and force
-        calculations. """
+    parent_calc: Object. Parent calculator used to query energy and force
+    calculations.
+"""
 
 
-def random_query(images, sample_candidates, samples_to_retrain, parent_calc):
+def random_query(_, sample_candidates, samples_to_retrain, parent_calc):
     """
     Randomly selects data points from a list of potential candidates to
     query and add to the existing training set.
     """
-    query_idx = random.sample(
-        range(1, len(sample_candidates)), samples_to_retrain
-    )
+    if len(sample_candidates) <= samples_to_retrain:
+        warnings.warn(
+            "# of samples exceeds # of available candidates! Defaulting to all available candidates",
+            stacklevel=2,
+        )
+        samples_to_retrain = len(sample_candidates) - 1
+    query_idx = random.sample(range(1, len(sample_candidates)), samples_to_retrain)
     images_to_query = [sample_candidates[idx] for idx in query_idx]
     queried_images = compute_query(images_to_query, parent_calc)
     return queried_images
 
 
-def max_uncertainty(images, sample_candidates, samples_to_retrain, parent_calc):
+def max_uncertainty(_, sample_candidates, samples_to_retrain, parent_calc):
     """Selects points with the largest uncertainty"""
-    uncertainty = np.array([atoms.info["uncertainty"][0] for atoms in sample_candidates])
-    query_idx = np.argpartition(uncertainty, -1*samples_to_retrain)[-1*samples_to_retrain:]
+    if len(sample_candidates) < samples_to_retrain:
+        warnings.warn(
+            "# of samples exceeds # of available candidates! Defaulting to all available candidates",
+            stacklevel=2,
+        )
+        samples_to_retrain = len(sample_candidates) - 1
+    uncertainty = np.array(
+        [atoms.info["uncertainty"][0] for atoms in sample_candidates]
+    )
+    query_idx = np.argpartition(uncertainty, -1 * samples_to_retrain)[
+        -1 * samples_to_retrain :
+    ]
     images_to_query = [sample_candidates[idx] for idx in query_idx]
     queried_images = compute_query(images_to_query, parent_calc)
     return queried_images
@@ -53,7 +70,7 @@ def compute_query(images_to_calculate, parent_calc):
     return queried_images
 
 
-def termination_criteria(termination_args, method='iter'):
+def termination_criteria(termination_args, method="iter"):
     """Criteria for AL termination
     Parameters
     ----------
