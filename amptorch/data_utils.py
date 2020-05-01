2c1ab96a5a15545f385ae120862250d12ac7ff08
Muhammed <mshuaibi@andrew.cmu.edu>
2020-05-01 14:38:46 -0400

    edge case when len(images)==1
    Former-commit-id: 398e52dd2cde1241db8412a8d079458bb58dd727

--- amptorch/data_utils.py
+++ amptorch/data_utils.py
@@ -5,8 +5,12 @@ class Transform():
     """Normalizes an input tensor and later reverts it.
     Adapted from https://github.com/Open-Catalyst-Project/baselines"""
     def __init__(self, tensor):
-        self.mean = torch.mean(tensor, dim=0)
-        self.std = torch.std(tensor, dim=0)
+        if len(tensor) > 1:
+            self.mean = torch.mean(tensor, dim=0)
+            self.std = torch.std(tensor, dim=0)
+        else:
+            self.mean = 0
+            self.std = 1
 
     def norm(self, tensor, energy=True):
         return (tensor - self.mean) / self.std if energy else tensor/self.std
