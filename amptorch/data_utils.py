fa9f00191200aaa27ed63f29cb6046a7a521d50d
Muhammed <mshuaibi@andrew.cmu.edu>
2020-08-02 17:53:01 -0400

    refactor scaling and model structure

--- amptorch/data_utils.py
+++ amptorch/data_utils.py
@@ -1,15 +1,29 @@
 import torch
 
 
-class Transform():
+class Normalize:
     """Normalizes an input tensor and later reverts it.
     Adapted from https://github.com/Open-Catalyst-Project/baselines"""
     def __init__(self, tensor):
         self.mean = torch.mean(tensor, dim=0)
         self.std = torch.std(tensor, dim=0)
 
-    def norm(self, tensor, energy=True):
-        return (tensor - self.mean) / self.std if energy else tensor/self.std
+    def norm(self, data_list, energy=True):
+        if energy:
+            for data in data_list:
+                data.energy = (data.energy - self.mean) / self.std
+        else:
+            for data in data_list:
+                data.force = data.force / self.std
 
-    def denorm(self, tensor, energy=True):
-        return tensor * self.std + self.mean if energy else tensor * self.std
+        return data_list
+
+    def denorm(self, data_list, energy=True):
+        if energy:
+            for data in data_list:
+                data.energy = (data.energy * self.std) + self.mean
+        else:
+            for data in data_list:
+                data.force = data.force * self.std
+
+        return data_list
