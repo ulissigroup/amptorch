f0fed42627847b9c5488823f57e888d766ecdb37
ray38 <xlei38@gatech.edu>
2020-08-21 20:14:12 -0400

    debug

--- amptorch/dataset.py
+++ amptorch/dataset.py
@@ -154,11 +154,11 @@ def sparse_block_diag(arrs):
     v = []
     r, c = 0, 0
     for k, (rr, cc) in enumerate(shapes):
-        print(r, rr, rr.numpy())
-        print(np.arange(r, r+rr.numpy()))
-        print(np.arange(c, c+cc.numpy()))
+        # print(r, rr, rr.numpy())
+        # print(np.arange(r, r+rr.numpy()))
+        # print(np.arange(c, c+cc.numpy()))
         # print(itertools.product(np.arange(r, r+rr.numpy()).astype(int), np.arange(c, c+cc.numpy()).astype(int)))
-        print(list(itertools.product(np.arange(r, r+rr.numpy()), np.arange(c, c+cc.numpy()))))
+        # print(list(itertools.product(np.arange(r, r+rr.numpy()), np.arange(c, c+cc.numpy()))))
         i += [
             torch.LongTensor(
                 list(itertools.product(np.arange(r, r+rr.numpy()), np.arange(c, c+cc.numpy())))
