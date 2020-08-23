f05bba448b279b340a343eb268ffe399e53df23d
ray38 <xlei38@gatech.edu>
2020-08-23 10:37:52 -0400

    added training data size match

--- amptorch/dataset.py
+++ amptorch/dataset.py
@@ -88,7 +88,7 @@ class AMPTorchDataset(Dataset):
                 if num_dummy_atoms > 0:
                     num_descriptors = (image_fp_list[0].shape)[1]
                     print("added dummy atoms: {} \t num descriptors: {}".format(num_dummy_atoms, num_descriptors))
-                    image_fp_list.append(np.zeros(num_dummy_atoms, num_descriptors))
+                    image_fp_list.append(np.zeros((num_dummy_atoms, num_descriptors)))
                     atomic_numbers += [0] * num_dummy_atoms
                     natoms += num_dummy_atoms
 
