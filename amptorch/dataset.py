1a51a672fb77bfecdf540da4c9ee8275cd915412
Muhammed <mshuaibi@andrew.cmu.edu>
2020-09-02 00:20:07 -0400

    cleans dataset and descriptor_calc

--- amptorch/dataset.py
+++ amptorch/dataset.py
@@ -14,73 +14,58 @@ import itertools
 class AMPTorchDataset(Dataset):
     def __init__(
         self,
-        trajs,
+        images,
         descriptor,
         automatic_calculation=True,
         force_calculation=True,
-        # calculate_descriptor_primes = True,
         sparse_prime=True,
         store_descriptors=True,
-        PCA_transform=False,
-        PCA_ncomponents=10,
-        scaling=True,
         training_data=False,
-        result_dir="./_results_/",
         parallel=False,
         cores=1,
     ):
 
+        self.images = images
         self.force_calculation = force_calculation
-        self.PCA_transform = PCA_transform
-        self.PCA_ncomponents = PCA_ncomponents
-        self.scaling = scaling
 
         self.descriptor_calculator = DescriptorCalculator(
-            trajs=trajs,
+            images=images,
             descriptor=descriptor,
             automatic_calculation=False,
             calculate_descriptor_primes=self.force_calculation,
             sparse_prime=sparse_prime,
             store_descriptors=store_descriptors,
             training_data=training_data,
-            result_dir=result_dir,
             parallel=parallel,
             cores=cores,
         )
 
-        if automatic_calculation:
-            self.process()
+        self.data_list = self.process()
 
     def process(self):
         self.descriptor_calculator.prepare_descriptors()
+        descriptor_data = self.descriptor_calculator._get_calculated_descriptors()
 
-        if self.PCA_transform:
-            self.descriptor_calculator.calculate_PCA(n_components=self.PCA_ncomponents)
-        if self.scaling:
-            self.descriptor_calculator.calculate_scaling()
-
-        sys.exit()
-        self._prepare_data()
-
-    def _prepare_data(self):
         data_list = []
-        raw_data = self.descriptor_calculator._get_calculated_descriptors()
-        element_list = self.descriptor_calculator.element_list
 
-        for idx, image_data in enumerate(raw_data):
-            potential_energy = image_data["energy"]
-            natoms = image_data["num_atoms"]
-            image_fingerprint = image_data["descriptors"]
-            atomic_numbers = image_data["atomic_numbers"]
+        for idx, image in enumerate(self.images):
+            energy = image.get_potential_energy(apply_constraint=False)
+            forces = image.get_forces(apply_constraint=False)
+            atomic_numbers = image.get_atomic_numbers()
+            natoms = len(image)
+            image_fingerprint = descriptor_data[idx]["descriptors"]
 
             image_fingerprint = torch.tensor(image_fingerprint)
             atomic_numbers = torch.tensor(atomic_numbers)
-            image_idx = torch.full((1, natoms), idx, dtype=torch.int64).view(-1)
+            image_idx = torch.full(
+                (1, natoms), idx, dtype=torch.int64
+            ).view(-1)
+
             data = Data(
                 fingerprint=image_fingerprint,
                 image_idx=image_idx,
                 atomic_numbers=atomic_numbers,
-                energy=potential_energy,
+                energy=energy,
                 natoms=natoms,
             )
             if self.force_calculation:
@@ -102,98 +87,13 @@ class AMPTorchDataset(Dataset):
 
             data_list.append(data)
 
-        self.data_length = len(data_list)
-        self.data = data_list
+        return data_list
 
     def __len__(self):
-        return self.data_length
+        return len(self.data_list)
 
     def __getitem__(self, index):
         return self.data[index]
-
-    # def _prepare_data(self, match_max_natoms = True):
-
-    #     data_list = []
-    #     raw_data = self.descriptor_calculator._get_calculated_descriptors()
-    #     element_list = self.descriptor_calculator.element_list
-
-    #     for idx, image_data in enumerate(raw_data):
-    #         potential_energy = image_data["energy"]
-    #         image_fp_list = []
-    #         atomic_numbers = []
-    #         natoms = 0
-
-    #         for element in element_list:
-    #             if element in image_data.keys():
-    #                 atomic_number = ATOM_SYMBOL_TO_INDEX_DICT[element]
-    #                 size_info = image_data[element]["size_info"]
-    #                 num_element = size_info[1]
-    #                 atomic_numbers += [atomic_number] * num_element
-    #                 natoms += num_element
-    #                 image_fp_list.append(image_data[element]["descriptors"])
-
-    #         image_fingerprint = torch.tensor(
-    #             np.vstack(image_fp_list)
-    #         )
-    #         atomic_numbers = torch.tensor(atomic_numbers)
-    #         image_idx = torch.full((1, natoms), idx, dtype=torch.int64).view(-1)
-    #         data = Data(
-    #             fingerprint=image_fingerprint,
-    #             image_idx=image_idx,
-    #             atomic_numbers=atomic_numbers,
-    #             energy=potential_energy,
-    #             natoms=natoms,
-    #         )
-
-    #         if self.force_calculation:
-    #             image_forces_list = []
-    #             image_fp_primes_list = []
-    #             for element in element_list:
-    #                 if element in image_data.keys():
-    #                     forces = image_data[element]["forces"]
-    #                     fp_prime_value = image_data[element]["descriptor_primes"]["value"]
-    #                     fp_prime_row   = image_data[element]["descriptor_primes"]["row"]
-    #                     fp_prime_col   = image_data[element]["descriptor_primes"]["col"]
-    #                     fp_prime_size  = image_data[element]["descriptor_primes"]["size"]
-
-    #                     element_fp_prime_matrix = coo_matrix((fp_prime_value, (fp_prime_row, fp_prime_col)), shape=fp_prime_size)
-
-    #                     image_forces_list.append(forces)
-    #                     image_fp_primes_list.append(element_fp_prime_matrix)
-
-    #             image_fp_primes = vstack(image_fp_primes_list)
-    #             # scipy_sparse_fp_prime.data, scipy_sparse_fp_prime.row, scipy_sparse_fp_prime.col, np.array(fp_prime.shape)
-    #             indices = np.vstack((image_fp_primes.row, image_fp_primes.col))
-    #             torch_indices = torch.LongTensor(indices)
-    #             torch_values = torch.FloatTensor(image_fp_primes.data)
-    #             fp_primes_torch_sparse = torch.sparse.FloatTensor(torch_indices, torch_values, torch.Size(image_fp_primes.shape))
-
-    #             image_forces = np.vstack(image_forces_list)
-
-    #             data.forces = torch.FloatTensor(image_forces)
-    #             data.fprimes = fp_primes_torch_sparse
-
-    #             # data.forces = torch.FloatTensor(image_forces)
-    #             # data.fprimes = fingerprintprimes
-
-    #         data_list.append(data)
-
-    #     self.data_length = len(data_list)
-    #     self.data = data_list
-
-    # def _get_max_natoms(self, raw_data, element_list):
-    #     max_natoms = 0
-    #     for idx, image_data in enumerate(raw_data):
-    #         natoms = 0
-    #         for element in element_list:
-    #             if element in image_data.keys():
-    #                 size_info = image_data[element]["size_info"]
-    #                 num_element = size_info[1]
-    #                 natoms += num_element
-    #         if max_natoms < natoms:
-    #             max_natoms = natoms
-    #     return max_natoms
-
 
 # Adapted from https://github.com/pytorch/pytorch/issues/31942
 def sparse_block_diag(arrs):
