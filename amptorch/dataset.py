cbda25e0fa851cee6402d77763ebdabf589cf5bc
Muhammed <mshuaibi@andrew.cmu.edu>
2020-09-14 01:49:34 -0400

    moves dataset preprocessing to its own script, allowing for flexibility in train/test time preprocessing

--- amptorch/dataset.py
+++ amptorch/dataset.py
@@ -1,10 +1,7 @@
-import numpy as np
-import torch
 from torch.utils.data import Dataset
-from torch_geometric.data import Batch, Data
+from torch_geometric.data import Batch
 
-from amptorch.data_utils import Normalize, sparse_block_diag
-from amptorch.descriptor.descriptor_calculator import DescriptorCalculator
+from amptorch.preprocessing import AtomsToData, Normalize, sparse_block_diag
 
 
 class AtomsDataset(Dataset):
@@ -12,59 +9,24 @@ class AtomsDataset(Dataset):
         self, images, descriptor, forcetraining=True, save_fps=True, cores=1,
     ):
         self.images = images
-        self.forcetraining = forcetraining
-
-        self.descriptor_calculator = DescriptorCalculator(
-            images=images,
+        self.a2d = AtomsToData(
             descriptor=descriptor,
-            calc_derivatives=forcetraining,
+            r_energy=True,
+            r_forces=True,
             save_fps=save_fps,
+            fprimes=forcetraining,
             cores=cores,
         )
 
         self.data_list = self.process()
 
     def process(self):
-        descriptor_data = self.descriptor_calculator.prepare_descriptors()
-        data_list = []
 
-        for idx, image in enumerate(self.images):
-            image_data = descriptor_data[idx]
-            energy = image.get_potential_energy(apply_constraint=False)
-            atomic_numbers = image.get_atomic_numbers()
-            natoms = len(image)
-            image_fingerprint = image_data["descriptors"]
+        data_list = self.a2d.convert_all(self.images)
 
-            image_fingerprint = torch.FloatTensor(image_fingerprint)
-            atomic_numbers = torch.LongTensor(atomic_numbers)
-            image_idx = torch.full((1, natoms), idx, dtype=torch.int64).view(-1)
-
-            data = Data(
-                fingerprint=image_fingerprint,
-                image_idx=image_idx,
-                atomic_numbers=atomic_numbers,
-                energy=energy,
-                natoms=natoms,
-            )
-            if self.forcetraining:
-                forces = image.get_forces(apply_constraint=False)
-                fp_prime_val = image_data["descriptor_primes"]["val"]
-                fp_prime_row = image_data["descriptor_primes"]["row"]
-                fp_prime_col = image_data["descriptor_primes"]["col"]
-                fp_prime_size = image_data["descriptor_primes"]["size"]
-
-                indices = np.vstack((fp_prime_row, fp_prime_col))
-                torch_indices = torch.LongTensor(indices)
-                torch_values = torch.FloatTensor(fp_prime_val)
-                fp_primes = torch.sparse.FloatTensor(
-                    torch_indices, torch_values, torch.Size(fp_prime_size)
-                )
-
-                data.forces = torch.FloatTensor(forces)
-                data.fprimes = fp_primes
-
-            data_list.append(data)
-
+        # Normalize fingerprints
+        # Normalize targets
+        # TODO: clean up normalization schemes
         self.normalizer = Normalize(data_list)
         data_list = self.normalizer.norm(data_list)
 
