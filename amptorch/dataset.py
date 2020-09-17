normalization bug fix, adds feature/target scaling

amptorch/dataset.py
@@ -1,7 +1,8 @@
 from torch.utils.data import Dataset
 from torch_geometric.data import Batch
 
-from amptorch.preprocessing import AtomsToData, Normalize, sparse_block_diag
+from amptorch.preprocessing import (AtomsToData, FeatureScaler, TargetScaler,
+                                    sparse_block_diag)
 
 
 class AtomsDataset(Dataset):
@@ -24,11 +25,10 @@ class AtomsDataset(Dataset):
 
         data_list = self.a2d.convert_all(self.images)
 
-        # Normalize fingerprints
-        # Normalize targets
-        # TODO: clean up normalization schemes
-        self.normalizer = Normalize(data_list)
-        data_list = self.normalizer.norm(data_list)
+        self.feature_scaler = FeatureScaler(data_list)
+        self.target_scaler = TargetScaler(data_list)
+        self.feature_scaler.norm(data_list)
+        self.target_scaler.norm(data_list)
 
         return data_list
 