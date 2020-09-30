096f8fb94fb358a9cd704137a08b15e8bf537209
Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
2020-09-30 15:03:25 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,59 +1,20 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages, Extension
-# To use a consistent encoding
-from codecs import open
-from os import path, listdir
-from pkg_resources import DistributionNotFound, get_distribution
-from subprocess import check_output
-
-install_requires = [
-    "torch",
-    "spglib",
-    "scikit-learn",
-    "skorch==0.6.0",
-    "scipy",
-    "pandas",
-    'numpy',
-    'ase>=3.10.0',
-    'cffi>=1.0.0',
-    'h5py',
-]
+from setuptools import setup, find_packages
 
 setup_requires = [
     'cffi>=1.0.0',
 ]
 
-
 setup(
     name="amptorch",
     version="0.1",
-    description="A Pytorch Implementation of AMP",
+    description="Atomistic Machine-learning Package - PyTorch",
     author="Muhammed Shuaibi, Xiangyun Lei",
     author_email="mshuaibi@andrew.cmu.edu, xlei38@gatech.edu",
     url="https://github.com/ulissigroup/amptorch",
-    classifiers=[   # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # other arguments are listed here.
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-
-    ],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    package_data={'':['*.cpp', '*.h']},
-    python_requires='>=2.7, <4',
-    install_requires=install_requires,
+    package_data={'': ['*.cpp', '*.h']},
+    python_requires='>=3.6, <4',
     setup_requires=setup_requires,
     cffi_modules=[
         "amptorch/descriptor/Gaussian/libsymf_builder.py:ffibuilder",
