847c020fe83c74ba8d016c100c880e0777d150f3
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2020-10-04 21:32:27 -0400

    Descriptor refactor [WIP] (#24)
    * added descriptor calculation
    
    * changed dataset
    
    * debug
    
    * updated example
    
    * added training data size match
    
    * refactor
    
    * debug
    
    * saved num descriptor in image_dict
    
    * remove non-skorch bits
    
    * remove old trainer
    
    * renaming
    
    * renaming methods, classes, directories. Fingerprints on a per-image basis rather than per-trajectory
    
    * descriptor test against amp
    
    * cleans dataset and descriptor_calc
    
    * element combination for G4, G5 changed, installation fixed
    
    * debug
    
    * removed compiled lib
    
    * added compiled lib to ignore
    
    * fixed installation bug
    
    * cleans up descriptor arguments/structure
    
    * modified gaussian fingerprint order to be consistent with amp
    
    * adds tqdm verbosity to fingerprints, cleans up structure
    
    * G2/G4 eta consistency with amp literature, updates and organizes new dataset, organize sparse block computation
    
    * includes trainer class. integrates pytorch geometric with skorch trainer
    
    * implements more efficient block sparse matrix construction, resolves scatter bug in model forward pass, updates normalization for targets and features, includes new trainer example
    
    * adds wandb logger, updates skorch epoch metrics
    
    * includes conda installation yaml, pre-commit configs
    
    * moves dataset preprocessing to its own script, allowing for flexibility in train/test time preprocessing
    
    * checkpoint logging/loading
    
    * normalization bug fix, adds feature/target scaling
    
    * predict function implementation (#26)
    
    * predict and minor bugfix
    
    * fix typo in dependencies
    
    * update gitignore and modify trainer example
    
    * updated setup
    
    * updated predict function, denorm energy/force scores, clean up naming
    
    Co-authored-by: Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>
    
    Co-authored-by: ray38 <xlei38@gatech.edu>
    Co-authored-by: Rui Qi Chen <38446772+ruiqic@users.noreply.github.com>
    
    Former-commit-id: 4fbcbcbdb5661296d517bd23fa61938c261fdef9

---
+++ amptorch/descriptor/Gaussian/symmetry_functions.h
@@ -0,0 +1,29 @@
+/*
+ Code for calculate symmetry function.
+ This code is used for both Python and LAMMPS code
+ */
+const int IMPLEMENTED_TYPE[] = {2, 4, 5}; // Change this when you implement new symfunc type!
+
+static inline double pow_int(const double &x, const double n) {
+    double res,tmp;
+
+    if (x == 0.0) return 0.0; // FIXME: abs(x) < epsilon
+    int nn = (n > 0) ? n : -n;
+    tmp = x;
+
+    for (res = 1.0; nn != 0; nn >>= 1, tmp *= tmp)
+        if (nn & 1) res *= tmp;
+
+    return (n > 0) ? res : 1.0/res;
+}
+
+double sigm(double, double &);
+double cutf(double);
+double dcutf(double, double);
+double G2(double, double *, double *, double &);
+double G4(double, double, double, double, double *, double *, double *);
+double G5(double, double, double, double *, double *, double *);
+
+double G2_noderiv(double, double *, double *, double &);
+double G4_noderiv(double, double, double, double, double *, double *, double *);
+double G5_noderiv(double, double, double, double *, double *, double *);
---
+++ amptorch/preprocessing/atoms_to_data.py
@@ -0,0 +1,126 @@
+import ase.db.sqlite
+import ase.io.trajectory
+import numpy as np
+import torch
+from torch_geometric.data import Data
+
+from amptorch.descriptor.descriptor_calculator import DescriptorCalculator
+
+try:
+    shell = get_ipython().__class__.__name__
+    if shell == "ZMQInteractiveShell":
+        from tqdm.notebook import tqdm
+    else:
+        from tqdm import tqdm
+except NameError:
+    from tqdm import tqdm
+
+
+class AtomsToData:
+    def __init__(
+        self,
+        descriptor,
+        r_energy=False,
+        r_forces=False,
+        save_fps=True,
+        fprimes=True,
+        cores=1,
+    ):
+        self.r_energy = r_energy
+        self.r_forces = r_forces
+        self.descriptor = descriptor
+        self.save_fps = save_fps
+        self.fprimes = fprimes
+        self.cores = cores
+
+    def convert(
+        self, atoms, idx,
+    ):
+        natoms = len(atoms)
+        image_data = self.descriptor_data[idx]
+        atomic_numbers = torch.LongTensor(atoms.get_atomic_numbers())
+        image_idx = torch.full((1, natoms), idx, dtype=torch.int64).view(-1)
+        image_fingerprint = torch.FloatTensor(image_data["descriptors"])
+
+        # put the minimum data in torch geometric data object
+        data = Data(
+            fingerprint=image_fingerprint,
+            image_idx=image_idx,
+            atomic_numbers=atomic_numbers,
+            natoms=natoms,
+        )
+
+        # optionally include other properties
+        if self.r_energy:
+            energy = atoms.get_potential_energy(apply_constraint=False)
+            data.energy = energy
+        if self.r_forces:
+            forces = torch.FloatTensor(atoms.get_forces(apply_constraint=False))
+            data.forces = forces
+        if self.fprimes:
+            fp_prime_val = image_data["descriptor_primes"]["val"]
+            fp_prime_row = image_data["descriptor_primes"]["row"]
+            fp_prime_col = image_data["descriptor_primes"]["col"]
+            fp_prime_size = image_data["descriptor_primes"]["size"]
+
+            indices = np.vstack((fp_prime_row, fp_prime_col))
+            torch_indices = torch.LongTensor(indices)
+            torch_values = torch.FloatTensor(fp_prime_val)
+            fp_primes = torch.sparse.FloatTensor(
+                torch_indices, torch_values, torch.Size(fp_prime_size)
+            )
+
+            data.fprimes = fp_primes
+
+        return data
+
+    def convert_all(
+        self, atoms_collection, disable_tqdm=False,
+    ):
+        """Convert all atoms objects in a list or in an ase.db to graphs.
+
+        Args:
+            atoms_collection (list of ase.atoms.Atoms or ase.db.sqlite.SQLite3Database):
+            Either a list of ASE atoms objects or an ASE database.
+
+        Returns:
+            data_list (list of torch_geometric.data.Data):
+            A list of torch geometric data objects containing molecular graph info and properties.
+        """
+
+        if isinstance(atoms_collection, list):
+            atoms_iter = atoms_collection
+        elif isinstance(atoms_collection, ase.db.sqlite.SQLite3Database):
+            atoms_iter = atoms_collection.select()
+        elif isinstance(
+            atoms_collection, ase.io.trajectory.SlicedTrajectory
+        ) or isinstance(atoms_collection, ase.io.trajectory.TrajectoryReader):
+            atoms_iter = atoms_collection
+        else:
+            raise NotImplementedError
+
+        descriptor_calculator = DescriptorCalculator(
+            images=atoms_collection,
+            descriptor=self.descriptor,
+            calc_derivatives=self.fprimes,
+            save_fps=self.save_fps,
+            cores=self.cores,
+        )
+        self.descriptor_data = descriptor_calculator.prepare_descriptors()
+
+        # list for all data
+        data_list = []
+        for idx, atoms in tqdm(
+            enumerate(atoms_iter),
+            desc="converting ASE atoms collection to Data objects",
+            total=len(atoms_collection),
+            unit=" systems",
+            disable=disable_tqdm,
+        ):
+            # check if atoms is an ASE Atoms object this for the ase.db case
+            if not isinstance(atoms, ase.atoms.Atoms):
+                atoms = atoms.toatoms()
+            data = self.convert(atoms, idx)
+            data_list.append(data)
+
+        return data_list
