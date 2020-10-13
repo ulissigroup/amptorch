ensemble trainer file sharing strategy, edge scale scaling fix, apply black

amptorch/active_learning/al_utils.py
@@ -41,9 +41,7 @@ def attach_sp_calc(images):
 def construct_sp(image):
     sample_energy = image.get_potential_energy(apply_constraint=False)
     sample_forces = image.get_forces(apply_constraint=False)
-    image.set_calculator(
-        sp(atoms=image, energy=sample_energy, forces=sample_forces)
-    )
+    image.set_calculator(sp(atoms=image, energy=sample_energy, forces=sample_forces))
     return image
 
 
@@ -53,21 +51,23 @@ def write_to_db(database, queried_images):
 
 
 def compute_loss(a, b):
-    return np.mean(np.sqrt(np.sum((a-b)**2, axis=1)))
+    return np.mean(np.sqrt(np.sum((a - b) ** 2, axis=1)))
 
 
-def progressive_plot(filename, true_relaxed, samples_per_iter, num_iterations, save_to="./"):
+def progressive_plot(
+    filename, true_relaxed, samples_per_iter, num_iterations, save_to="./"
+):
     os.makedirs(save_to, exist_ok=True)
     distance_rmse = []
-    data_size = list(range(0, samples_per_iter*num_iterations+1, samples_per_iter))
+    data_size = list(range(0, samples_per_iter * num_iterations + 1, samples_per_iter))
 
     for i in range(len(data_size)):
         ml_relaxed = ase.io.read("{}_iter_{}.traj".format(filename, i), "-1")
         loss = compute_loss(ml_relaxed.positions, true_relaxed.positions)
         distance_rmse.append(loss)
-    plt.plot(np.array(data_size)+1, distance_rmse)
+    plt.plot(np.array(data_size) + 1, distance_rmse)
     plt.xlabel("Training Images")
-    plt.xticks(np.array(data_size)+1)
+    plt.xticks(np.array(data_size) + 1)
     plt.ylabel("Distance RMSE")
     plt.title("AL Relaxation Learning Curve")
-    plt.savefig(save_to+".png", dpi=300)
+    plt.savefig(save_to + ".png", dpi=300)

amptorch/active_learning/trainer.py
@@ -4,6 +4,8 @@ from torch.multiprocessing import Pool
 
 import torch
 
+torch.multprocessing.set_sharing_strategy("file_system")
+
 from skorch import NeuralNetRegressor
 from skorch.dataset import CVSplit
 from skorch.callbacks import Checkpoint, EpochScoring, LRScheduler
@@ -148,7 +150,7 @@ def model_trainer(images, training_params):
                 fn_prefix="./results/checkpoints/{}_".format(filename),
             ),
             load_best_valid_loss,
-            LRScheduler
+            LRScheduler,
         ]
 
     net = NeuralNetRegressor(

amptorch/data_utils.py
@@ -5,7 +5,7 @@ class Transform():
     """Normalizes an input tensor and later reverts it.
     Adapted from https://github.com/Open-Catalyst-Project/baselines"""
     def __init__(self, tensor):
-        if len(tensor) > 1:
+        if len(torch.unique(tensor)) > 1:
             self.mean = torch.mean(tensor, dim=0)
             self.std = torch.std(tensor, dim=0)
         else: