8fb60c642064620b71ce2f22b56c91fdd22834a9
ray38 <xlei38@gatech.edu>
2020-10-16 09:08:52 -0400

    force training test

--- amptorch/descriptor/MCSH/__init__.py
+++ amptorch/descriptor/MCSH/__init__.py
@@ -196,7 +196,7 @@ class AtomisticMCSH(BaseDescriptor):
 
         threshold = 1e-15
         super_threshold_indices_prime = np.abs(fp_prime) < threshold
-        print("threshhold: {} \tnum points set to zero:{} \t outof: {}".format(threshold, np.sum(super_threshold_indices), fp_prime.shape[0] * fp_prime.shape[1]))
+        print("threshhold: {} \tnum points set to zero:{} \t outof: {}".format(threshold, np.sum(super_threshold_indices_prime), fp_prime.shape[0] * fp_prime.shape[1]))
         fp_prime[super_threshold_indices_prime] = 0.0
 
         scipy_sparse_fp_prime = sparse.coo_matrix(fp_prime)
