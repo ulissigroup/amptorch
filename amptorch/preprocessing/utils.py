7d8a9f565c6de3c2246db06c83a2f0f46f326907
ray38 <xlei38@gatech.edu>
2020-10-16 10:47:12 -0400

    force training test

--- amptorch/preprocessing/utils.py
+++ amptorch/preprocessing/utils.py
@@ -18,9 +18,9 @@ class FeatureScaler:
             self.feature_mean = torch.mean(fingerprints, dim=0)
             self.feature_std = 1
 
-    def norm(self, data_list):
+    def norm(self, data_list, threshold = 1e-6):
         for data in data_list:
-            idx_to_scale = torch.where((self.feature_max - self.feature_min) > 10e-8)[0]
+            idx_to_scale = torch.where((self.feature_max - self.feature_min) > threshold)[0]
             data.fingerprint[:, idx_to_scale] = -1 + 2 * (
                 (data.fingerprint[:, idx_to_scale] - self.feature_min[idx_to_scale])
                 / (self.feature_max[idx_to_scale] - self.feature_min[idx_to_scale])
@@ -35,7 +35,7 @@ class FeatureScaler:
                 #     - self.feature_min[idx_to_scale_prime]
                 # )[0]
 
-                nonzero_idx = torch.where((self.feature_max - self.feature_min) > 10e-8)[0]
+                nonzero_idx = torch.where((self.feature_max - self.feature_min) > threshold)[0]
 
                 print("=====================================")
                 print(data.fprimes)
