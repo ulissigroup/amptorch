5642d2d481a6ea92fdd0cee776720addaf32dc7b
ray38 <xlei38@gatech.edu>
2020-10-15 22:38:04 -0400

    force training test

--- amptorch/descriptor/MCSH/calculate_atomistic_mcsh.cpp
+++ amptorch/descriptor/MCSH/calculate_atomistic_mcsh.cpp
@@ -274,13 +274,13 @@ extern "C" int calculate_atomistic_mcsh(double** cell, double** cart, double** s
                     dMdx = (1/M) * (sum_miu1 * sum_dmiu1_dxj[j] + sum_miu2 * sum_dmiu2_dxj[j] + sum_miu3 * sum_dmiu3_dxj[j]) * weight;
                     dMdy = (1/M) * (sum_miu1 * sum_dmiu1_dyj[j] + sum_miu2 * sum_dmiu2_dyj[j] + sum_miu3 * sum_dmiu3_dyj[j]) * weight;
                     dMdz = (1/M) * (sum_miu1 * sum_dmiu1_dzj[j] + sum_miu2 * sum_dmiu2_dzj[j] + sum_miu3 * sum_dmiu3_dzj[j]) * weight;
-                    // dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3] += dMdx;
-                    // dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3 + 1] += dMdy;
-                    // dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3 + 2] += dMdz;
+                    dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3] += dMdx;
+                    dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3 + 1] += dMdy;
+                    dmcsh[ii*nmcsh + m][nei_list_i[j*2 + 1]*3 + 2] += dMdz;
 
-                    // dmcsh[ii*nmcsh + m][i*3]     -= dMdx;
-                    // dmcsh[ii*nmcsh + m][i*3 + 1] -= dMdy;
-                    // dmcsh[ii*nmcsh + m][i*3 + 2] -= dMdz;
+                    dmcsh[ii*nmcsh + m][i*3]     -= dMdx;
+                    dmcsh[ii*nmcsh + m][i*3 + 1] -= dMdy;
+                    dmcsh[ii*nmcsh + m][i*3 + 2] -= dMdz;
                 }
                 M = M * weight;
                 mcsh[ii][m] += M;
