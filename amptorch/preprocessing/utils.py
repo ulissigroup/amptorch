44d526b4a6907b077bda61e33e88ff5a8d4ad5d1
ray38 <xlei38@gatech.edu>
2020-10-16 11:30:33 -0400

    force training test

--- amptorch/preprocessing/utils.py
+++ amptorch/preprocessing/utils.py
@@ -40,6 +40,7 @@ class FeatureScaler:
                 print("=====================================")
                 print(data.fprimes)
                 print(torch.max(torch.abs(data.fprimes._values())))
+                print((self.feature_max - self.feature_min))
                 
                 # print(self.feature_max[idx_to_scale_prime][nonzero_idx])
                 # print(self.feature_min[idx_to_scale_prime][nonzero_idx])
