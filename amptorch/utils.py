4dfbc0c98007891a93313db7f8ae9bd4122816b5
Eric Musa <ericmusa@umich.edu>
2020-10-22 13:46:07 -0400

    copied Peterson's Polynomial cutoff function from Amp

--- amptorch/utils.py
+++ amptorch/utils.py
@@ -593,6 +593,7 @@ def dict2cutoff(dct):
                            ' "name" and "kwargs".')
     return globals()[dct['name']](**dct['kwargs'])
 
+                
 class Cosine(object):
     """Cosine functional form suggested by Behler.
 
@@ -648,3 +649,71 @@ class Cosine(object):
     def __repr__(self):
         return ('<Cosine cutoff with Rc=%.3f from amp.descriptor.cutoffs>'
                 % self.Rc)
+
+
+class Polynomial(object):
+    """Polynomial functional form suggested by Khorshidi and Peterson.
+
+    Parameters
+    ----------
+    gamma : float
+        The power of polynomial.
+    Rc : float
+        Radius above which neighbor interactions are ignored.
+    """
+
+    def __init__(self, Rc, gamma=4):
+        self.gamma = gamma
+        self.Rc = Rc
+
+    def __call__(self, Rij):
+        """
+        Parameters
+        ----------
+        Rij : float
+            Distance between pair atoms.
+
+        Returns
+        -------
+        value : float
+            The value of the cutoff function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            value = 1. + self.gamma * np.power(Rij / self.Rc, self.gamma+1) - \
+                (self.gamma + 1) * np.power(Rij / self.Rc, self.gamma)
+            return value
+
+    def prime(self, Rij):
+        """Derivative (dfc_dRij) of the Polynomial cutoff function with respect to Rij.
+
+        Parameters
+        ----------
+        Rij : float
+            Distance between pair atoms.
+
+        Returns
+        -------
+        float
+            The value of derivative of the cutoff function.
+        """
+        if Rij > self.Rc:
+            return 0.
+        else:
+            value = (self.gamma * (self.gamma + 1) / self.Rc) * \
+                (np.power(Rij / self.Rc, self.gamma) -
+                 np.power(Rij / self.Rc, self.gamma - 1))
+            return value
+
+    def todict(self):
+        return {'name': 'Polynomial',
+                'kwargs': {'Rc': self.Rc,
+                           'gamma': self.gamma
+                           }
+                }
+
+    def __repr__(self):
+        return ('<Polynomial cutoff with Rc=%.3f and gamma=%i '
+                'from amp.descriptor.cutoffs>'
+                % (self.Rc, self.gamma))
