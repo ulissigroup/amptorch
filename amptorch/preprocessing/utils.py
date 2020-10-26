BUGFIX - Feature scaling breaks with length ==1. Turns off feature scaling for now. (#55)
* bug? add fingerprints for single image only

* removes feature scaling until bugfix

Co-authored-by: Muhammed Shuaibi <mshuaibi@andrew.cmu.edu>

amptorch/preprocessing/utils.py
@@ -7,20 +7,15 @@ class FeatureScaler:
 
     def __init__(self, data_list, forcetraining):
         self.forcetraining = forcetraining
+        fingerprints = torch.cat([data.fingerprint for data in data_list], dim=0)
+        self.feature_max = torch.max(fingerprints, dim=0).values
+        self.feature_min = torch.min(fingerprints, dim=0).values
 
-        if len(data_list) > 1:
-            fingerprints = torch.cat([data.fingerprint for data in data_list], dim=0)
-
-            self.feature_max = torch.max(fingerprints, dim=0).values
-            self.feature_min = torch.min(fingerprints, dim=0).values
-
-        else:
-            self.feature_mean = torch.mean(fingerprints, dim=0)
-            self.feature_std = 1
-
-    def norm(self, data_list, threshold = 1e-6):
+    def norm(self, data_list, threshold=1e-6):
         for data in data_list:
-            idx_to_scale = torch.where((self.feature_max - self.feature_min) > threshold)[0]
+            idx_to_scale = torch.where(
+                (self.feature_max - self.feature_min) > threshold
+            )[0]
             data.fingerprint[:, idx_to_scale] = -1 + 2 * (
                 (data.fingerprint[:, idx_to_scale] - self.feature_min[idx_to_scale])
                 / (self.feature_max[idx_to_scale] - self.feature_min[idx_to_scale])
@@ -30,7 +25,11 @@ class FeatureScaler:
                     data.fingerprint.shape[1] - 1
                 )
                 nonzero_idx = torch.where(
-                    (self.feature_max[idx_to_scale_prime] - self.feature_min[idx_to_scale_prime]) > threshold
+                    (
+                        self.feature_max[idx_to_scale_prime]
+                        - self.feature_min[idx_to_scale_prime]
+                    )
+                    > threshold
                 )[0]
                 data.fprimes._values()[nonzero_idx] *= 2 / (
                     self.feature_max[idx_to_scale_prime][nonzero_idx]

amptorch/trainer.py
@@ -88,10 +88,9 @@ class AtomsTrainer:
             save_fps=self.config["dataset"].get("save_fps", True),
         )
 
-        self.feature_scaler = self.train_dataset.feature_scaler
         self.target_scaler = self.train_dataset.target_scaler
         if not self.debug:
-            normalizers = {"feature": self.feature_scaler, "target": self.target_scaler}
+            normalizers = {"target": self.target_scaler}
             torch.save(normalizers, os.path.join(self.cp_dir, "normalizers.pt"))
         self.input_dim = self.train_dataset.input_dim
         self.val_split = self.config["dataset"].get("val_split", 0)
@@ -203,7 +202,6 @@ class AtomsTrainer:
         )
 
         data_list = a2d.convert_all(images, disable_tqdm=True)
-        self.feature_scaler.norm(data_list)
 
         self.net.module.eval()
         collate_fn = DataCollater(train=False, forcetraining=self.forcetraining)