eb3a99827e77204d2bc275d039e0856fbbd5ce43
Eric Musa <ericmusa@umich.edu>
2020-12-03 22:45:37 -0500

    what happens if I retroactively add these commits?

---
+++ amptorch/tests/gaussian_descriptor_set_test.py
@@ -0,0 +1,24 @@
+# TODO
+#
+#
+#
+# Gs = {
+#     "default": {
+#         "G2": {
+#             "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4),
+#             "rs_s": [0],
+#         },
+#         "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
+#         "cutoff": 6,
+#     },
+# }
+#
+# pass Gs to GaussianDescriptorSet
+# pass Gs to Gaussian
+#
+# show that GDS.to_descriptor_setup() == Gaussian.descriptor_setup
+#
+#
+#
+#
+#
