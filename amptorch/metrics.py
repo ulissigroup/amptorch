d81bc5c913aff2d37e65b18414b291fb5dfb2ca9
ray38 <xlei38@gatech.edu>
2021-02-23 18:21:24 -0500

    turned on force score saving

--- amptorch/metrics.py
+++ amptorch/metrics.py
@@ -125,10 +125,10 @@ def evaluator(
                     target_extractor=target_extractor,
                 )
             )
-        # callbacks.append(
-        #     Checkpoint(
-        #         monitor="{}_forces_{}_best".format(cp_on, metric),
-        #         fn_prefix="checkpoints/{}/".format(identifier),
-        #     )
-        # )
+        callbacks.append(
+            Checkpoint(
+                monitor="{}_forces_{}_best".format(cp_on, metric),
+                fn_prefix="checkpoints/{}/".format(identifier),
+            )
+        )
     return callbacks
